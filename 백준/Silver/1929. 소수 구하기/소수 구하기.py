27e07eb46247d4975cdce94410ea02993a002bfc
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2023-02-03 01:37:35 +0900

    [Silver III] Title: 소수 구하기, Time: 3164 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -4,16 +4,12 @@ m, n = map(int, input().split())
 def primality(n):
     if n == 1:
         return False
-    i = 2
-    while i * i <= n:
+    for i in range(2, int(n**0.5)+1):
         if n % i == 0:
             return False
-        i += 1
     return True
 
 # 입력 받은 범위 내에서 소수 판별
 for num in range(m, n+1):
     if primality(num):
         print(num)
-    else:
-        continue
