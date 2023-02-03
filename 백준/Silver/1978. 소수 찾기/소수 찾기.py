255539ea850d2c0408ee2d07c1f2a6e6d4017517
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2023-02-03 13:33:05 +0900

    [Silver V] Title: 소수 찾기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,21 @@
+import sys
+n = int(input())
+num = list(map(int, sys.stdin.readline().split()))
+cnt = 0
+
+# 소수 판별 함수
+def primality(n):
+    if n == 1:
+        return False
+    i = 2
+    while i * i <= n:
+        if n % i == 0:
+            return False
+        i += 1
+    return True
+
+for i in num:
+    if primality(i):
+        cnt += 1
+        
+print(cnt)
