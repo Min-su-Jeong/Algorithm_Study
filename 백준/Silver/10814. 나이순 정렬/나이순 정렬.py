68bcf00341f24da1514e97645996508364c81064
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2023-02-09 18:24:20 +0900

    [Silver V] Title: 나이순 정렬, Time: 272 ms, Memory: 46276 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,10 +1,11 @@
+import sys
 n = int(input())
 lst = []
 
 for _ in range(n):
-    age, name = map(str, input().split())
+    age, name = map(str, sys.stdin.readline().split())
     lst.append((int(age), name))   
 lst.sort(key = lambda x : x[0])
 
 for i in lst:
-    print(i[0], i[1])
+    print(*i, sep=" ")
