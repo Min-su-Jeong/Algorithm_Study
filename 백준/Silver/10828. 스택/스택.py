bc4f1d646bde4e20208ed1368a947a2057225fd7
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2023-02-10 09:17:09 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+stack = []
+
+for _ in range(int(input())):
+    com = input().split()
+    if com[0] == 'push':
+        stack.append(com[1])
+        
+    elif com[0] == 'pop':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack.pop())
+            
+    elif com[0] == 'size':
+        print(len(stack))
+        
+    elif com[0] == 'empty':
+        if len(stack) == 0:
+            print(1)
+        else:
+            print(0)
+            
+    elif com[0] == 'top':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[-1])
