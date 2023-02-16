7e9edd05909837e2faa70a4226c4fd10261f7763
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2023-02-16 22:22:09 +0900

    [Silver II] Title: 마인크래프트, Time: 420 ms, Memory: 124336 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -1,23 +1,28 @@
 import sys
 input = sys.stdin.readline
+
+time, height = -1, -1
 n, m, b = map(int, input().split())
-G, sol = [], [] # ground, Solution
 
+G = [] # ground
 for _ in range(n):
-    G += (map(int, input().split())) # 1차원으로 저장
+    G.extend(list(map(int, input().split()))) # 1차원으로 저장
+G.sort() # 오름차순 정렬
 
-for h in range(min(G), max(G)+1): # 최소/최대 범위에서만 반복 수행
-    t, inv = 0, b # 시간, 인벤토리
+for h in range(G[0], G[-1]+1): # 최소/최대 범위에서만 반복 수행
+    t, inv = 0, b
     for g in G:
         if g > h: # 목표 높이보다 좌표의 높이가 큰 경우: 블록 제거
             t += 2*(g-h)
             inv += g-h
-        elif g < h: # 목표 높이보다 좌표의 높이가 작은 경우: 블록 쌓기
+        else: # 목표 높이보다 좌표의 높이가 작은 경우: 블록 쌓기
             t += h-g
             inv -= h-g
     
-    if inv>=0:
-        sol.append([t, h])
+    if inv < 0: # 블록이 더이상 없는 경우
+        break
+    
+    if time == -1 or t <= time: # 여러 개의 답 중 땅의 높이가 가장 높은 것 찾기
+        time, height = t, h
         
-sol.sort(key=lambda x:(x[0],-x[1])) # 걸린시간 오름차순, 높이 내림차순
-print(sol[0][0],sol[0][1])
+print(time, height)
