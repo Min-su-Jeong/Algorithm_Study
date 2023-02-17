b7c000a4e8304e107aad9a4012083c3aed58e272
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2023-02-17 12:03:05 +0900

    [Gold V] Title: 리모컨, Time: 1120 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+cnt = abs(100 - n) # 채널 100번부터 시작
+m = int(input())
+
+if m: # 고장이 있을 경우에만 입력 받기
+    btn = set(input().split())
+else:
+    btn = set()
+
+for i in range(1000000): 
+    # 각 숫자별 고장 확인 후, 고장 시 break
+    for j in str(i):
+        if j in btn:
+            break
+
+    # 최소 이동 비교
+    else:
+        cnt = min(cnt, len(str(i)) + abs(i - n))
+
+print(cnt)
