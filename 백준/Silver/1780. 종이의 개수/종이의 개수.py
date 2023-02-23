60906ee69eace598825b7ed89df3d7d0b4f78c8a
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2023-02-23 23:02:46 +0900

    [Silver II] Title: 종이의 개수, Time: 5092 ms, Memory: 69484 KB -BaekjoonHub

---
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.py
@@ -0,0 +1,24 @@
+n = int(input())
+paper = [list(map(int, input().split())) for _ in range(n)]
+neg, neu, pos = 0, 0, 0
+
+def clip_paper(x, y, n):
+    global neg, neu, pos
+    num_check = paper[x][y]
+    for i in range(x, x + n):
+        for j in range(y, y + n):
+            if(paper[i][j] != num_check):
+                for k in range(3):
+                    for l in range(3):
+                        clip_paper(x + k * n//3, y + l * n//3, n//3)
+                return
+            
+    if(num_check == -1):
+        neg += 1
+    elif(num_check == 0):
+        neu += 1
+    else:
+        pos += 1
+        
+clip_paper(0, 0, n)
+print(f'{neg}\n{neu}\n{pos}')
