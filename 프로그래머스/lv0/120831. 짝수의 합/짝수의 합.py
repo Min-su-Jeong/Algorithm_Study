75910c58967d7c742fbf537277272eed396f4915
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2023-03-19 13:49:50 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.py
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.py
@@ -1,7 +1,2 @@
 def solution(n):
-    ans = 0
-    for i in range(n+1): # n 이하의 수까지 반복
-        if i % 2 == 0:   # 해당 숫자가 짝수인 경우
-            ans += i     # 누적 합
-            
-    return ans
+    return sum([i for i in range(2, n + 1, 2)]) # n 이하의 수를 2씩 증가시킨 값(짝수)들의 합
