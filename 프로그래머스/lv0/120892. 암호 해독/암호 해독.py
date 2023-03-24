df4c97ca52bf50406b9739e464388a8f43c79610
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2023-03-25 02:25:56 +0900

    [level 0] Title: 암호 해독, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
@@ -1,2 +1,2 @@
 def solution(cipher, code):
-    return ''.join([cipher[i] for i in range(code-1, len(cipher), code)]) # code 배수에 해당되는 인덱스를 뽑아서 반환
+    return cipher[code-1::code] # code 배수에 해당되는 인덱스를 뽑아서 반환
