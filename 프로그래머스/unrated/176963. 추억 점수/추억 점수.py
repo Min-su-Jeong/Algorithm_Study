722e712c93d05215f3c46ab68635f98269a29b79
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2023-04-11 10:27:38 +0900

    [unrated] Title: 추억 점수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
@@ -1,13 +1,3 @@
 def solution(name, yearning, photo):
-    res, dic = [], {}            # 결과, 딕셔너리 변수
-    for i, n in enumerate(name): # name 요소와 인덱스 반환
-        dic[n] = yearning[i]     # 이름과 추억 점수를 딕셔너리 형태로 변환
-        
-    for p in photo:                
-        score = 0                  # 추억 점수 초기화
-        for name in p:             # 이름 하나씩 꺼내기
-            if name in dic:        # 딕셔너리 내에 이름이 존재하면
-                score += dic[name] # 해당하는 value 값을 score에 더하기
-        res.append(score)          # 리스트에 점수 추가
-        
-    return res
+    # 2개의 for문을 통해 이름을 하나씩 접근 / 만약, 이름이 존재하면 해당하는 추억 점수 인덱스에 접근하여 합 구하기
+    return [sum(yearning[name.index(i)] for i in p if i in name) for p in photo]
