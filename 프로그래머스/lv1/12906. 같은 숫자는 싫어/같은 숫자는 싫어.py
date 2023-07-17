044116f057e91c0a24c12087564ffc03276ac204
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2023-07-17 17:29:43 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 43.07 ms, Memory: 27.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,11 +1,10 @@
 def solution(arr):
-    tmp = -1
-    res = []
-    for a in arr:
-        if a != tmp: # 숫자가 연속적이지 않으면
-            tmp = a  # tmp를 a 값으로 정의
-            res.append(tmp) # res에 tmp 값 추가
-        else:
-            pass
-        
-    return res
+    result = []
+    cur_num = -1
+    
+    for i in arr:
+        if cur_num != i:
+            result.append(i)
+            cur_num = i
+            
+    return result
