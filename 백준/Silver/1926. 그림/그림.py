1c4c07e0e0e1792df03ea901f77bd86c8d59cadf
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2023-08-16 16:55:55 +0900

    [Silver I] Title: 그림, Time: 320 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -2,35 +2,34 @@ import sys
 from collections import deque
 
 def bfs(x, y):
+    area = 1
+    graph[x][y] = 0
     q = deque()
     q.append((x, y))
-    graph[x][y] = 0
-    size = 1
     while q:
         x, y = q.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny]:
+            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 1:
                 graph[nx][ny] = 0
                 q.append((nx, ny))
-                size += 1
-                
-    return size
+                area += 1
+    return area
 
 input = sys.stdin.readline
 n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0 , -1, 1]
+dx = [-1, 1, 0 ,0]
+dy = [0, 0, -1, 1]
 
-cnt, max_size = 0, 0
+cnt, max_area = 0, 0
 for i in range(n):
     for j in range(m):
-        if graph[i][j]:
+        if graph[i][j] == 1:
             cnt += 1
-            max_size = max(bfs(i, j), max_size)
-        
+            max_area = max(bfs(i, j), max_area)
+
 print(cnt)
-print(max_size)
+print(max_area)
