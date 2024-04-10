8caa168b749b974f741df01a7262e3b4e63bf934
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2024-04-10 21:49:27 +0900

    [Gold V] Title: 리모컨, Time: 1132 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.py
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -1,26 +1,32 @@
-import sys
-input = sys.stdin.readline
-n = int(input())
-cnt = abs(100 - n) # 채널 100번부터 시작
-m = int(input())
+"""
+1.전략
+완전탐색
+N의 범위 내 채널을 증가 or 감소시킴
+버튼으로 누를 수 없는 채널인 경우 pass
+누를 수 있는 채널인 경우, 대수 비교(최솟값 찾기)
+"result = min(100~N번까지 눌러야 하는 횟수, 가장 가까운 채널로 이동해서 + or - 누르는 횟수)"
 
-if n == 100:
-    print(0)
-    sys.exit(0)
+2.시간복잡도
+O(N * M) = 500,001 * 6 = 3,000,006
+=> 2초 내 가능
+"""
 
-if m: # 고장이 있을 경우에만 입력 받기
-    btn = set(input().split())
+N = int(input()) # 수빈이가 이동하려고 하는 채널
+M = int(input()) # 고장난 버튼의 개수
+if M:            # 고장난 버튼이 있는 경우만 입력 받기
+    broken = set(input().split())
 else:
-    btn = set()
+    broken = set()
 
-for i in range(1000000): 
-    # 각 숫자별 고장 확인 후, 고장 시 break
-    for j in str(i):
-        if j in btn:
+MAX = 1000000       # 채널 - or + 을 고려한 범위(0~999,999)
+result = abs(100-N) # 결과 변수
+
+for channel in range(MAX+1): # 범위 내 완전 탐색
+    for c in str(channel):   # Channel의 숫자를 하나씩 분할하여 검증
+        if c in broken:      # 누를 수 없는 채널인 경우 break
             break
-
-    # 최소 이동 비교
     else:
-        cnt = min(cnt, len(str(i)) + abs(i - n))
-
-print(cnt)
+        # 최소 버튼 클릭 횟수 구하기
+        result = min(result, len(str(channel)) + abs(channel-N))
+        
+print(result)
