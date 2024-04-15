fef126300d12a2736375dd8289f062a9803c377b
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2024-04-16 08:35:26 +0900

    [Silver III] Title: N과 M (3), Time: 2004 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,22 @@
+"""
+1.전략
+- 백트래킹
+
+2.시간복잡도
+O(N^2) = 8 * 8 = 64
+=> 1초 내 가능
+"""
+def backTracking(curNum):
+    if len(arr) == M:
+        print(*arr)
+        return
+    
+    for i in range(1, N+1):
+        arr.append(i)
+        backTracking(curNum+1)
+        arr.pop()
+        
+N, M = map(int, input().split())
+arr = []
+
+backTracking(0)
