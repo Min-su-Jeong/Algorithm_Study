0d2d50e2ab91e38f08af08a26ae884668aebf2a6
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2024-04-18 23:39:28 +0900

    [Silver I] Title: NM과 K (1), Time: 3520 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/18290. NM과 K （1）/NM과 K （1）.py
+++ 백준/Silver/18290. NM과 K （1）/NM과 K （1）.py
@@ -8,7 +8,7 @@
 - O((N*M)^K) = (10*10)^4 = 100,000,000
   => 2초 이내 가능
 """
-def dfs(selectNum, sumNum):
+def dfs(cx, cy, selectNum, sumNum):
     global res
     
     # 선택된 개수가 K개가 되면 종료
@@ -17,36 +17,36 @@ def dfs(selectNum, sumNum):
         return
 
     # NxM 배열 순회
-    for i in range(N):
-        for j in range(M):
-            # [i, j] 번째 수를 방문하지 않은 경우
-            if not visited[i][j]:
-                visited[i][j] += 1
+    for x in range(cx, N):
+        cy = 0 if x != cx else cy
+        for y in range(cy, M):
+            visitFlag = False
+            
+            # [x, y] 좌표에 해당하는 수를 방문했는지 여부 확인
+            if visited[x][y]:
+                continue
                 
-                # (i,j) 기준 좌표에서 상,하,좌,우 방문 여부 확인
-                for k in range(4):
-                    nx, ny = i + dx[k], j + dy[k]
-                    if 0 <= nx < N and 0 <= ny < M:
-                        visited[nx][ny] += 1
+            # (x, y) 좌표 기준에서 상,하,좌,우 방문 여부 확인
+            for k in range(4):
+                nx, ny = x + dx[k], y + dy[k]
+                if 0 <= nx < N and 0 <= ny < M and visited[nx][ny]:
+                    visitFlag = True # 하나라도 있으면 True
                         
-                # parameter: (선택 수 +1, (i,j)번째 수를 sumNum에 더한 값)
-                dfs(selectNum+1, sumNum+graph[i][j])
-                
-                # 재귀함수가 모든 구역을 탐색한 후, 인접한 구역 해제
-                visited[i][j] -= 1
-                for k in range(4):
-                    nx, ny = i + dx[k], j + dy[k]
-                    if 0 <= nx < N and 0 <= ny < M:
-                        visited[nx][ny] -= 1
+            # 방문한 적이 없는 경우, 순회 시작
+            if not visitFlag:
+                visited[x][y] = True
+                dfs(x, y, selectNum+1, sumNum+graph[x][y])
+                visited[x][y] = False
 
+                
 res = -40000 # 입력받는 최솟 값 : -10000 => K는 최대 4이므로 -10000*4=-40000=최솟 값          
 N, M, K = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(N)]
-visited = [[0]*M for _ in range(N)]
+visited = [[False]*M for _ in range(N)]
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-dfs(0, 0)
+dfs(0, 0, 0, 0)
 
 print(res)
