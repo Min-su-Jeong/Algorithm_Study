5070c9f3753190f91f8bb457eeda8e22257459d7
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2024-04-28 00:10:06 +0900

    [Silver III] Title: 모든 순열, Time: 180 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10974. 모든 순열/모든 순열.py
@@ -0,0 +1,29 @@
+"""
+1,전략
+- DFS
+- visited 리스트: 방문 여부 확인
+- res 리스트: 순회하면서 N 길이만큼 채워질 때마다 return
+
+2.시간복잡도
+- O(N^2): 8 ^ 2 = 64 (Worst case)
+  => 1초 이내 가능
+"""
+def dfs(depth: int):
+    if len(res) == N:
+        print(*res)
+        return
+    
+    for i in range(1, N+1):
+        if not visited[i]:
+            visited[i] = True
+            res.append(i)
+            dfs(depth+1)
+            visited[i] = False
+            res.pop()
+            
+        
+N = int(input())
+visited = [False] * (N+1)
+res = []
+
+dfs(0)
