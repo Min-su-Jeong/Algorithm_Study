09933ca34d72514b9a990aa32b2496ee6295c75c
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2024-05-15 22:23:44 +0900

    [Silver II] Title: 연속합, Time: 84 ms, Memory: 38828 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -1,7 +1,33 @@
-n = int(input())
-m = list( map(int, input().split(' ')))
- 
-for i in range(1, n):
-    m[i] = max(m[i], m[i] + m[i-1])
+"""
+1.전략
+- DP(Dynamic Programming)
+- 각 원소(i)마다 앞의 부분합을 구할 필요 X
+- 점화식 : dp[i] = max(arr[i], dp[i-1] + arr[i])
+
+2.시간복잡도
+- O(N) = 100,000(Worst case)
+  => 1초 이내 가능
+"""
+import sys; input = sys.stdin.readline
+
+def DP(N: int, arr: list):
+    global dp
     
-print(max(m))
+    for i in range(1, N):
+        dp[i] = max(arr[i], dp[i-1] + arr[i])
+
+    return max(dp)
+
+# 입력받기
+N = int(input())
+arr = list(map(int, input().split()))
+
+# 필요한 변수 선언 및 초기화
+dp = [min(arr)] * N
+dp[0] = arr[0]
+
+# 함수 실행
+res = DP(N, arr)
+
+# 결과 출력
+print(res)
