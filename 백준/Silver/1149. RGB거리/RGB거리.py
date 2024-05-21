860d7e8f40d0959f3ebe7d025ae87122cf30b865
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2024-05-21 09:19:08 +0900

    [Silver I] Title: RGB거리, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,12 +1,34 @@
-n = int(input())
-a = [0]*n
+"""
+1.전략
+- DP(Dynamic Programming)
+- 첫 행을 첫번째 집에 각 색을 칠하는 비용으로 채워넣은 후,
+- 이전 집에서 칠한 색을 제외한 색 중 더 저렴한 색을 칠하면 되는 문제
 
-for i in range(n):
-    a[i] = list(map(int,input().split()))
-    
-for i in range(1,n):
-    a[i][0]= min(a[i-1][1],a[i-1][2]) + a[i][0]
-    a[i][1]= min(a[i-1][0],a[i-1][2]) + a[i][1]
-    a[i][2]= min(a[i-1][0],a[i-1][1]) + a[i][2]
+2.시간복잡도
+- O(N) = 1,000
+  => 0.5초 이내 가능
+"""
+import sys; input = sys.stdin.readline
 
-print(min(a[n-1][0],a[n-1][1],a[n-1][2]))
+def DP(N: int):
+  for i in range(1, N):
+    # 현재 열을 제외한 i-1행의 열들 중 최솟 값 + 현재 칸 비용
+    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + arr[i][0]
+    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + arr[i][1]
+    dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + arr[i][2]
+
+  return min(dp[N-1])
+
+# 입력 받기
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+
+# 필요한 변수 선언 및 초기화
+dp = [[0] * 3 for _ in range(N)]
+dp[0] = arr[0]
+
+# 함수 실행
+res = DP(N)
+
+# 결과 출력
+print(res)
