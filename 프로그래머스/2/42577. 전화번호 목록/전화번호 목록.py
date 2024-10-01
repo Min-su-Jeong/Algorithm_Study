db8894c52b87853d304e3a7e0aa8472869cff8f9
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2024-10-01 15:30:10 +0900

    [level 2] Title: 전화번호 목록, Time: 470.17 ms, Memory: 46.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,8 +1,14 @@
 def solution(phoneBook):
-    phoneBook.sort()
-
-    for p1, p2 in zip(phoneBook, phoneBook[1:]):
-        if p2.startswith(p1):
-            return False
+    hashMap = {}
+    for pb in phoneBook:
+        hashMap[pb] = True
         
+    for pb in phoneBook:
+        arr = ''
+        for s in pb:
+            arr += s
+
+            if arr in hashMap and arr != pb:
+                return False
+            
     return True
