1c3c16743acc633298c7ce3c0acdcf16977d85f2
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2024-11-10 19:45:20 +0900

    [Silver III] Title: N과 M (3), Time: 1748 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,22 +1,37 @@
 """
 1.전략
-- 백트래킹
+- 백트래킹(Back Tracking)
+- list를 한 개 만들어 관리. DFS + 가지치기 추가
+- 중복 O: 방문 여부를 확인할 필요 X
 
-2.시간복잡도
-O(N^2) = 8 * 8 = 64
-=> 1초 내 가능
+2.시간 복잡도
+- 시간 제한: 1초
+- O(N^2) = 8 * 8 = 64
 """
-def backTracking(curNum):
+import sys
+input = sys.stdin.readline
+
+# Input
+N, M = map(int, input().split())
+
+# Variable
+graph = [i for i in range(1, N+1)]
+arr = []
+
+# Function
+def dfs(curNum: int):
+    global arr
+
+    # 종료 조건
     if len(arr) == M:
         print(*arr)
         return
-    
+
+    # 방문 조건
     for i in range(1, N+1):
         arr.append(i)
-        backTracking(curNum+1)
+        dfs(curNum+1)
         arr.pop()
-        
-N, M = map(int, input().split())
-arr = []
 
-backTracking(0)
+# Main
+dfs(0)
