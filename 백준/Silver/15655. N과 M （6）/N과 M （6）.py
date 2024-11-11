f4a995ed9cabf22a3fa8035651eee18712384997
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2024-11-11 23:08:37 +0900

    [Silver III] Title: N과 M (6), Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.py
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -1,31 +1,42 @@
 """
 1.전략
-- 백트래킹
-- 15654번과의 차이점: n번째 수보다 n+1번째 수가 더 커야하며 뒤에 있어야 함.
-- 비교하기 위해 현재 숫자 값을 가지는 변수를 추가
+- 백트래킹(Back Tracking)
+- list를 한 개 만들어 관리. DFS + 가지치기 추가
+- 중복 x: 방문 여부를 확인 필요
+- 수열 오름차순: 입력받은 숫자를 정렬 후 순회 시작
+- 시작 값을 기준으로 증가하는 순서여야 하므로 index를 curNum+1을 기준으로 설정
 
-2.시간복잡도
-O(N^2) = 8 * 8 = 64
-=> 1초 내 가능
+2.시간 복잡도
+- 시간 제한: 1초
+- O(N^M)
 """
-def backTracking(curNum: int):
-    if len(arr) == M:
-        print(*arr)
-        return
-    
-    for i, num in enumerate(nums):
-        if not visited[i] and curNum < num:
-            arr.append(num)
-            visited[i] = True
-            backTracking(num)
-            arr.pop()
-            visited[i] = False
-        
+import sys
+input = sys.stdin.readline
+
+# Input 
 N, M = map(int, input().split())
-nums = list(map(int, input().split()))
-nums.sort()
+arr = list(map(int, input().split()))
+arr.sort()
 
-arr = []
-visited = [False] * len(nums)
+# Variable
+res = []
+visited = [False for _ in range(N+1)]
 
-backTracking(0)
+# Solution
+def dfs(curNum: int): 
+    # 가지치기
+    if len(res) == M:
+        print(*res)
+        return
+
+    # 조건
+    for i in range(curNum+1, N+1):
+        if not visited[i]:
+            res.append(arr[i-1])
+            visited[i] = True
+            dfs(i)
+            res.pop()
+            visited[i] = False
+
+# Main
+dfs(0)
