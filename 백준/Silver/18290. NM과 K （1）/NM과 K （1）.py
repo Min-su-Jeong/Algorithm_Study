074af9d701ab8b6a1d52ee5c39a2d29d046f2615
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2024-11-12 23:11:35 +0900

    [Silver I] Title: NM과 K (1), Time: 3028 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/18290. NM과 K （1）/NM과 K （1）.py
+++ 백준/Silver/18290. NM과 K （1）/NM과 K （1）.py
@@ -1,52 +1,59 @@
 """
 1.전략
-- DFS
-- 재귀함수를 통해 K개를 선택 가능한 조합 체크
-- K개가 선택된 경우, 이전 조합에서의 최댓값과 비교하여 res에 갱신
+- 백트래킹(Back Tracking)
+- DFS + 가지치기
+- N X M 크기만큼 2차원 반복문 생성
+- 현재/인접 위치 방문 여부 확인 -> 모두 해당되지 않을 때 현재 위치 방문 표시
 
-2.시간복잡도
-- O((N*M)^K) = (10*10)^4 = 100,000,000
-  => 2초 이내 가능
+2.시간 복잡도
+- 시간 제한: 2초
+- O((N*M)^K) = (10 * 10) ^ 4 = 100,000,000
 """
-def dfs(cx, cy, selectNum, sumNum):
-    global res
-    
-    # 선택된 개수가 K개가 되면 종료
-    if selectNum == K:
-        res = max(res, sumNum)
-        return
+import sys
+input = sys.stdin.readline
 
-    # NxM 배열 순회
-    for x in range(cx, N):
-        cy = 0 if x != cx else cy
-        for y in range(cy, M):
-            visitFlag = False
-            
-            # [x, y] 좌표에 해당하는 수를 방문했는지 여부 확인
-            if visited[x][y]:
-                continue
-                
-            # (x, y) 좌표 기준에서 상,하,좌,우 방문 여부 확인
-            for k in range(4):
-                nx, ny = x + dx[k], y + dy[k]
-                if 0 <= nx < N and 0 <= ny < M and visited[nx][ny]:
-                    visitFlag = True # 하나라도 있으면 True
-                        
-            # 방문한 적이 없는 경우, 순회 시작
-            if not visitFlag:
-                visited[x][y] = True
-                dfs(x, y, selectNum+1, sumNum+graph[x][y])
-                visited[x][y] = False
-
-                
-res = -40000 # 입력받는 최솟 값 : -10000 => K는 최대 4이므로 -10000*4=-40000=최솟 값          
+# Input
 N, M, K = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(N)]
-visited = [[False]*M for _ in range(N)]
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+# Variables
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+visited = [[False for _ in range(M)] for _ in range(N)]
+res = -sys.maxsize
 
-dfs(0, 0, 0, 0)
+# Solution
+def backTracking(cx, cy, cost, depth):
+    global res
 
+    # 가지치기
+    if depth == K:
+        res = max(res, cost)
+        return
+
+    # 조건
+    for x in range(cx, N):
+        # 같은 행 중복 선택 방지
+        for y in range(cy if x == cx else 0, M):
+            # 현재 위치 방문여부 확인
+            if visited[x][y]:
+                continue
+            
+            # 인접 위치 방문여부 확인
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if 0 <= nx < N and 0 <= ny < M and visited[nx][ny]:
+                    break
+            
+            # 인접 위치 방문하지 않은 경우 방문 표시 및 순회
+            else:
+                visited[x][y] = True
+                backTracking(x, y, cost+graph[x][y], depth+1)
+                visited[x][y] = False
+
+# Main
+backTracking(0, 0, 0, 0)
+
+# Output
 print(res)
