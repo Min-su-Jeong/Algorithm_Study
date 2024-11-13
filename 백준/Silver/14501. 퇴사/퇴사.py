6af795ab7dce9e3b3bc02a6858cd7cc55756f10b
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2024-11-14 00:46:38 +0900

    [Silver III] Title: 퇴사, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,28 +1,32 @@
 """
 1.전략
-- DP(Dynamic Programming)
-- Case 1. i일 상담 X : dp[i+1] 
-  => 지난 상담까지의 이익
-- Case 2. i일 상담 O : dp[t[i]+i] + p[i] 
-  => dp[i일 + i일에 시작할 상담에 필요한 일 수] + 이익
-  
-2. 시간복잡도
-- 완전탐색의 경우 : O(2^N) = 2^15 = 32,768
-- DP의 경우 : O(N) = 15
-  => 2초 이내 가능
-"""
-import sys; input = sys.stdin.readline
+- 다이나믹 프로그래밍(Dynamic Programming)
+- 상담이 가능한지 여부부터 체크 => 날짜가 뒤로 갈수록 상담일이 퇴사일을 초과하는 경우가 많아짐
+- 이에 따라, 리스트를 뒤에서부터 체크하며 가능 여부 조건 분기
+- Case 1. i일 상담 X : dp[i+1] => 지난 상담까지의 이익
+- Case 2. i일 상담 O : dp[t[i]+i] + p[i] => dp[i일 + i일에 시작할 상담에 필요한 일 수] + 이익
 
+2.시간 복잡도
+- 제한 시간: 2초
+- O(N) = 15
+"""
+import sys
+input = sys.stdin.readline
+
+# Input
 N = int(input())
-counsel = [list(map(int, input().split())) for _ in range(N)]
-dp = [0] * (N+1)
+schedule = [list(map(int, input().split())) for _ in range(N)]
+
+# Variables
+dp = [0 for _ in range(N+1)]
 
 for i in reversed(range(N)):
-    # i일 상담 기간이 최사일을 초과하는 경우 상담 X
-    if i + counsel[i][0] > N:
-        dp[i] = dp[i+1]
+    # 상담 O: i일 기준 상담일이 퇴사일 전인 경우
+    if i + schedule[i][0] <= N:
+        dp[i] = max(dp[i+1], schedule[i][1] + dp[i+schedule[i][0]])
+    
+    # 상담 X: i일 기준 상담일이 퇴사일을 초과하는 경우
     else:
-        # i일에 상담 하는 것과 안하는 것 중 큰 것 선택
-        dp[i] = max(dp[i+1], counsel[i][1] + dp[i + counsel[i][0]])
-        
+        dp[i] = dp[i+1]
+
 print(dp[0])
