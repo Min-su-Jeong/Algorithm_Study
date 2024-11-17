c5792ecdeeb0871da3f5a009876e134f0eb39955
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2024-11-17 23:24:22 +0900

    [Silver III] Title: 다음 순열, Time: 40 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/10972. 다음 순열/다음 순열.py
+++ 백준/Silver/10972. 다음 순열/다음 순열.py
@@ -1,32 +1,31 @@
 """
 1.전략
-- 브루트포스
-- 1. 작은 수가 앞에 배치되어 있는지 확인(뒤에서부터 탐색 시작)
-- 2. 인덱스 값이 앞에 값보다 큰 값이 있다면 그 값과 Swap
-- 3. Swap을 반복 후, i에 해당하는 인덱스부터 오름차순 정렬 + 이어 붙이기
+- 수학
+- 뒤에서부터 시작
+- 1) 뒷 값이 앖 값보다 큰 경우 찾기 
+- 2) i-1 인덱스 값이 j 인덱스보다 작은 경우 => Swap
+- 3) 뒷 값에 해당하는 인덱스부터 마지막까지 오름차순 정렬
 
-2. 시간복잡도
-- O(N^2) = 10,000 ^ 2 = 100,000,000 (Worst Case)
-  => 1초 이내 가능
+2.시간 복잡도
+- 시간 제한: 1초
+- O(N^2) = 10,000 * 10,000 = 100,000,000
 """
-import sys; 
+import sys
 input = sys.stdin.readline
 
+# Input
 N = int(input())
-arr = list(map(int, input().split()))
+S = list(map(int, input().split()))
 
+# Solution
 for i in range(N-1, 0, -1):
-    # 뒷 값이 앞 값보다 큰 경우
-    if arr[i-1] < arr[i]:
+    if S[i-1] < S[i]:                       # 1) 뒷 값이 앞 값보다 큰 경우
         for j in range(N-1, 0, -1):
-            # i-1 인덱스 값이 j 인덱스보다 작은 경우
-            if arr[i-1] < arr[j]:
-                # Swap
-                arr[i-1], arr[j] = arr[j], arr[i-1]
-                # index: i 이후 값들을 오름차순 정렬 후 리스트화 
-                arr = arr[:i] + sorted(arr[i:])
-                print(*arr)
+            if S[i-1] < S[j]:               # 2) i-1 인덱스 값이 j 인덱스보다 작은 경우
+                S[i-1], S[j] = S[j], S[i-1] # 2) Swap
+                S = S[:i] + sorted(S[i:])   # 3) 뒷 값에 해당하는 인덱스부터 마지막까지 오름차순 정렬
+                print(*S)
                 exit()
-    
+
 # 마지막 순열인 경우
 print(-1)
