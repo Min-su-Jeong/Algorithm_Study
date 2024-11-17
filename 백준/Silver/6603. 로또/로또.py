721b60accbdee2db0a4edd3a12eb2a6583eebfd0
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2024-11-17 18:53:41 +0900

    [Silver II] Title: 로또, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.py
+++ 백준/Silver/6603. 로또/로또.py
@@ -1,15 +1,40 @@
-import itertools
+"""
+1.전략
+- 백트래킹(Back Tracking)
+- [오름차순 + 중복 X]인 수열 만들기 문제와 유사
+- 0이 나오기 전까지 테스트 케이스 연속 출력
+
+2.시간 복잡도
+- 시간 제한: 1초
+- O(N^6) = 12^6 = 2,985,984
+"""
+import sys
+input = sys.stdin.readline
+
+# Solution
+def backTracking(curNum: int):
+    global arr
+
+    if len(arr) == 6:
+        print(*arr)
+        return
+    
+    for i in range(curNum, K):
+        arr.append(S[i])
+        backTracking(i+1)
+        arr.pop()
 
 while True:
+    # Input
+    T = list(map(int, input().split()))
+    K, S = T[0], T[1:]
+    
+    # Variables
+    arr = []
 
-    array = list(map(int, input().split()))
-
-    k = array[0]
-    S = array[1:]
-
-    for i in itertools.combinations(S, 6):
-        print(*i)
-
-    if k == 0:
-        exit()
-    print()
+    # Main
+    if K == 0:
+        break
+    else:
+        backTracking(0)
+        print()
