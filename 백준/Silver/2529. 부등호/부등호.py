ba87fd7e325b778ad71bb0c1acda365a241c8883
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2024-11-17 22:05:42 +0900

    [Silver I] Title: 부등호, Time: 120 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.py
+++ 백준/Silver/2529. 부등호/부등호.py
@@ -1,15 +1,28 @@
 """
 1.전략
-- DFS
-- 숫자문자열들이 담긴 res 배열 => 오름차순 정렬
-- 첫번째와 마지막 인덱스를 나눠 출력
+- 백트래킹(Back Tracking)
+- 0 ~ 9 범위에서 중복 없는 수열 만들기
+- 수열을 만들 때 부등호 조건에 만족할 경우에만 순회
+- 조건에 충족하는 문자열 수열들을 res 변수에 저장
+- res의 값에서 맨 마지막(최댓값)과 맨 첫번째(최솟값)를 출력
 
-2.시간복잡도
-- check 함수 : O(1)
-- dfs 함수 : O(10^k) = 10^8 = 100,000,000
-  => 1초 이내 가능
+2.시간 복잡도
+- 시간 제한: 1초
+- O(10^N) = 10^8 = 100,000,000 
 """
-def check(a, b, op):
+import sys
+input = sys.stdin.readline
+
+# Input
+k = int(input())
+sign = list(input().split())
+
+# Variables
+res = []
+visited = [False for _ in range(10)]
+
+# Function 1. 부등호 관계 만족 여부 확인 함수
+def check(a, b, op) -> bool:
     if op == '<':
         if a > b: 
             return False
@@ -19,32 +32,26 @@ def check(a, b, op):
         
     return True
 
-def dfs(cnt, num):
-    if cnt == k+1:
-        res.append(num)
+# Function 2. 조건에 만족하는 최대/최소 값 구하는 함수
+def backTracking(curNum: int, curStr: str):
+    global res
+
+    if curNum == k+1:
+        res.append(curStr)
         return
-  
+
     for i in range(10):
-        if visited[i]: 
+        if visited[i]:
             continue
 
-        if cnt == 0 or check(num[cnt-1], str(i), signs[cnt-1]):
-            visited[i] = 1
-            dfs(cnt+1, num+str(i))
-            visited[i] = 0
+        if curNum == 0 or check(curStr[curNum-1], str(i), sign[curNum-1]):
+            visited[i] = True
+            backTracking(curNum+1, curStr+str(i))
+            visited[i] = False
 
-# 입력 받기
-k = int(input())
-signs = list(input().split())
+# Main
+backTracking(0, '')
 
-# 파라미터 초기화
-res = []
-visited = [0]*10
-
-# 순회 시작
-dfs(0, '')
-res.sort()
-
-# 결과 출력
+# Output
 print(res[-1])
 print(res[0])
