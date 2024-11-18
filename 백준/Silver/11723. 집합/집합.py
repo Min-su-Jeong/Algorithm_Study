18079de3206aba46565da7fd4f519baca75d5fd4
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2024-11-19 00:25:08 +0900

    [Silver V] Title: 집합, Time: 3596 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,40 +1,42 @@
-
 """
 1.전략
-- 비트마스크
-- shift 연산을 사용하여 문제 해결
-- 메모리를 효과적으로 해결하기 위한 방법
+- 비트마스크(Bit Mask)
+- 집합 대신 2진수를 사용
+- Shift 연산을 사용하여 명령어 구현 => 메모리 사용 최소화
 
-2. 시간 복잡도
-- M(N) = 3,000,000 (Worst Case)
+2.시간 복잡도
+- 시간 제한: 1.5초
+- O(N) = 3,000,000
 """
-import sys; input = sys.stdin.readline
+import sys
+input = sys.stdin.readline
 
+# Input
 M = int(input())
+
+# Variables
 S = 0b0
+
+# Solution
 for _ in range(M):
-    commands = input().rstrip()
+    cmd = input().rstrip().split()
 
-    try:
-        cmd, x = commands.split()
-        x = int(x)
-        if cmd == "add":
-            S  = S | (0b1 << x)
-
-        elif cmd == "remove":
-            S = S & ~(0b1 << x)
-
-        elif cmd == "check":
-            if (S & (0b1 << x)):
-                print(1)
-            else:
-                print(0)
-                
-        elif cmd == "toggle":
-            S = S ^ (0b1 << x)
-
-    except:
-        if commands == "all":
+    if len(cmd) == 1:
+        if cmd[0] == "all":
             S = 0b111111111111111111111
-        elif commands == "empty":
+        if cmd[0] == "empty":
             S = 0b0
+        continue
+
+    cmd, num = cmd[0], int(cmd[1])
+    if cmd == "add":
+        S |= (0b1 << num)
+    if cmd == "remove":
+        S &= ~(0b1 << num)
+    if cmd == "check":
+        if S & (0b1 << num):
+            print(1)
+        else:
+            print(0)
+    if cmd == "toggle":
+        S ^= (0b1 << num) 
