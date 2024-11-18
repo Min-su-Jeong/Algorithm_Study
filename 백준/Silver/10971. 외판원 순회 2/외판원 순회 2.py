86cb9db954c55d1596d62dd295e1e6b96bc437da
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2024-11-18 23:39:08 +0900

    [Silver II] Title: 외판원 순회 2, Time: 444 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -1,42 +1,43 @@
 """
 1.전략
-- DFS
-- 도시를 순회하면서 방문 여부와 도시 비용을 체크
-- 비용의 누적합을 구하며 가장 최소가 되는 비용을 res 값에 저장
+- 백트래킹(Back Tracking)
+- 1 ~ N번까지의 도시를 한번씩 순회하려면 각 행에서 한 번씩만 선택되어야 함.
+- 또한, 선택된 행과 열([i,i]) 기준으로 다음 행부터 방문된 열(i)은 갈 수 없음.
+- visited: 방문된 열 확인, cost: 누적된 비용 계산, depth: 순회 횟수
+- res 값과 비교를 통해 최소 비용 값을 구함
 
-2.시간복잡도
-- O(N!) = 10! = 3,628,800 (Worst case)
-  => 2초 이내 가능
+2.시간 복잡도
+- 시간 제한: 2초
+- O(N!) = 10! = 3,628,800
 """
-import sys; input = sys.stdin.readline
+import sys
+input = sys.stdin.readline
 
-def dfs(depth: int, cost: int, cur: int):
-    global graph, visited, res
-    
-    # N-1 : 모든 도시를 순회한 상태
-    # graph[cur][0] : 마지막 지점에서 1로 돌아가는 경로가 존재
-    if depth == N-1 and graph[cur][0]:
-        res = min(res, cost+graph[cur][0])
-        return
-    
-    for i in range(N):
-        # 방문여부와 행렬의 성분이 0인지 확인
-        if not visited[i] and graph[cur][i]:
-            visited[i] = True
-            dfs(depth+1, cost+graph[cur][i], i)
-            visited[i] = False
-
-# 입력받기
+# Input
 N = int(input())
 graph = [list(map(int, input().split())) for _ in range(N)]
 
-# 변수 선언
-visited = [False] * N
-res = int(1e9)
+# Variables
+res = sys.maxsize
+visited = [False for _ in range(N)]
 visited[0] = True
 
-# 합수 실행
-dfs(0, 0, 0)
+# Solution
+def backTacking(curNum: int, cost: int, depth: int):
+    global res
 
-# 결과 출력
+    if depth == N-1 and graph[curNum][0]:
+        res = min(res, cost+graph[curNum][0])
+        return
+
+    for i in range(N):
+        if not visited[i] and graph[curNum][i]:
+            visited[i] = True
+            backTacking(i, cost+graph[curNum][i], depth+1)
+            visited[i] = False
+
+# Main
+backTacking(0, 0, 0)
+
+# Output
 print(res)
