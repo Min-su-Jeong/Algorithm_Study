b06e010985c221863c552578d02cde1a10bdb537
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2024-11-20 10:24:57 +0900

    [Silver II] Title: 부분수열의 합, Time: 444 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,38 +1,38 @@
 """
 1.전략
-- 백트래킹(Back tracking)
-- 부분 수열을 찾아야하므로 조합이 필요하다고 판단
-- 조합 + 조건에 맞는 경우를 찾기 위해서 수열을 순회하면서 부합하는 경우 결과+1
+- 백트래킹(Back Tracking)
+- 중복 X, 사전 순으로 순회하는 DFS 구현
+- 가지치기 = 부분 수열의 합이 S와 같아지는 경우 
 
 2.시간 복잡도
-- O(2^N) = 2 ^ 20 = 1,048,576 (Worst case)
-  => 2초 이내 가능
+- 시간 제한: 2초
+- O(2^N) = 2 ^ 20 = 1,048,576
 """
-import sys; input = sys.stdin.readline
+import sys
+input = sys.stdin.readline
 
-def backTracking(start_idx: int):
+# Input
+N, S = map(int, input().split())
+seq = list(map(int, input().split()))
+
+# Variables
+res = 0
+arr = []
+
+# Solution
+def backTracking(curNum:int):
     global res
 
-    # 합이 S인 경우 & 부분 수열의 길이가 1 이상인 경우
     if sum(arr) == S and len(arr) > 0:
         res += 1
-    
-    # 순회(중복 X) -> start_idx부터 시작
-    for i in range(start_idx, N):
+
+    for i in range(curNum, N):
         arr.append(seq[i])
         backTracking(i+1)
         arr.pop()
 
-# 입력
-N, S = map(int, input().split())
-seq = list(map(int, input().split()))
-
-# 필요한 변수 선언 및 초기화
-res = 0
-arr = []
-
-# 함수 실행
+# Main
 backTracking(0)
 
-# 출력
+# Output
 print(res)
