4c5c7fc9d316cde07f1ec401fde19a1f52933360
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2024-11-23 21:46:49 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -1,27 +1,35 @@
 """
 1.전략
-- DP(Dynamic Programming)
-- 1~8 뒤에 올 수 있는 숫자는 2종류 / 9는 오직 8만 가능
-- 구조 : dp[자릿 수][앞에 오는 숫자] = 경우의 수
+- 다이나믹 프로그래밍(Dynamic Programming)
+- 크게 3가지의 규칙이 있음
+  => 1. 0: 0을 제외한 나머지는 시작 숫자로 지정 가능
+  => 2. 1~8: 뒤에 올 수 있는 숫자가 총 2종류
+  => 3. 9: 뒤에 8만 올 수 있음
+- 점화식
+  => 0인 경우: dp[i][0] = dp[i-1][1]
+  => 1~8인 경우: dp[i][Num] = dp[i-1][Num-1] + dp[i-1][Num+1]
+  => 9인 경우: dp[i][9] = dp[i-1][8]
 
-  1) 앞에 오는 숫자 = 0
-  dp[자릿 수][0] = dp[자릿 수 - 1][1]
-
-  2) 앞에 오는 숫자 = 1~8
-     dp[자릿 수][1-8] 
-     = dp[자릿 수 - 1][앞에 오는 숫자 -1] + dp[자릿 수 - 1][앞에 오는 숫자 + 1]
- 
-  3) 앞에 오는 숫자 = 9
-     dp[자릿 수][9] = dp[자릿 수 - 1][8]
-
-2.시간복잡도
-- O(N) = 100,000
-  => 1초 이내 가능
+2.시간 복잡도
+- 시간 제한: 1초
+- O(N)
 """
-def DP(N: int):
+import sys
+input = sys.stdin.readline
+
+# Input
+N = int(input())
+
+# Variables
+dp = [[0 for _ in range(10)] for _ in range(N+1)]
+for i in range(1, 10):
+    dp[1][i] = 1
+    
+# Solution
+def DP(n: int):
     global dp
 
-    for i in range(2, N+1):
+    for i in range(2, n+1):
         for j in range(10):
             if j == 0:
                 dp[i][j] = dp[i-1][1]
@@ -29,17 +37,8 @@ def DP(N: int):
                 dp[i][j] = dp[i-1][8]
             else:
                 dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
-                
-    return sum(dp[N]) % MOD
 
-# 입력 받기(테스트 케이스 수)
-N = int(input())
+    return sum(dp[N]) % 1000000000
 
-# 필요한 변수 선언 및 초기화
-MOD = 1000000000
-dp = [[0] * 10 for _ in range(N+1)]
-for i in range(1, 10):
-    dp[1][i] = 1
-
-# 함수 실행 및 결과 출력
+# Output
 print(DP(N))
