0f30a7b70ae4b1daa6b9d5f20241d82fafc0c969
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2024-11-23 19:41:16 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 128 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.py
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.py
@@ -5,30 +5,31 @@
 - j: i보다 작은 카드의 장수
 - j와 미지수 x의 합이 i가 나와야하기 때문에 
 - j + x = i 라는 식을 만족시켜야 함 => x = i - j (i > j)
-- 11052번 : 카드 구매하기 문제에서 반대의 경우로 생각하기
 
 2.시간복잡도
-- O(N) = 10,000
+- O(N^2) = 1,000 * 1,000 = 1,000,000
   => 1초 이내 가능
 """
-import sys; input = sys.stdin.readline
+import sys
+input = sys.stdin.readline
 
-def DP():
-    global dp, N
-    
-    for i in range(1, N+1):
-        for j in range(1, i+1):
-            dp[i] = min(dp[i], dp[i-j] + Pn[j])
-            
-    return dp[N]
-
-# 입력 받기
+# Input
 N = int(input())
-Pn = [0] + list(map(int, input().split()))
+cost = [0] + list(map(int, input().split()))
 
-# 필요한 변수 선언 및 초기화
-dp = [10000] * (N+1)
+# Variables
+dp = [10000 for _ in range(N+1)]
 dp[0] = 0
 
-# 함수 실행 및 결과 출력
-print(DP())
+# Solution
+def DP(n) -> int:
+    global dp
+
+    for i in range(1, n+1):
+        for j in range(1, i+1):
+            dp[i] = min(dp[i], dp[i-j] + cost[j])
+
+    return dp[n]
+
+# Output
+print(DP(N))
