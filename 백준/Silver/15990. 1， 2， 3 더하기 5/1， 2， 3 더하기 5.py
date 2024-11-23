c48528c7f0e41247587ce80713e1bb0e97cb9217
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2024-11-23 20:31:36 +0900

    [Silver I] Title: 1, 2, 3 더하기 5, Time: 196 ms, Memory: 55584 KB -BaekjoonHub

--- 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.py
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.py
@@ -14,32 +14,31 @@
 - O(N) = 100,000
   => 1초 이내 가능
 """
-def DP():
-    global dp
+import sys
+input = sys.stdin.readline
 
-    for i in range(4, MAX):
-        dp[i][0] = (dp[i-1][1] + dp[i-1][2]) % MOD
-        dp[i][1] = (dp[i-2][0] + dp[i-2][2]) % MOD
-        dp[i][2] = (dp[i-3][0] + dp[i-3][1]) % MOD
-
-# 입력 받기(테스트 케이스 수)
+# Input
 T = int(input())
 
-# 필요한 변수 선언 및 초기화
-MOD = 1000000009
+# Variables
 MAX = 100001
-dp = [[0] * 3 for _ in range(MAX)]
+MOD = 1000000009
+dp = [[0 for _ in range(3)] for _ in range(MAX)]
 dp[1] = [1, 0, 0]
 dp[2] = [0, 1, 0]
 dp[3] = [1, 1, 1]
 
-# 함수 실행
-DP()
+# Solution
+for i in range(4, MAX):
+    dp[i][0] = dp[i-1][1] + dp[i-1][2] % MOD
+    dp[i][1] = dp[i-2][0] + dp[i-2][2] % MOD
+    dp[i][2] = dp[i-3][0] + dp[i-3][1] % MOD
 
+# Main
 for i in range(T):
-    # 입력 받기 (n)
+    # Input
     n = int(input())
-    
-    # 결과 계산 및 출력
+
+    # Output
     res = sum(dp[n]) % MOD
     print(res)
