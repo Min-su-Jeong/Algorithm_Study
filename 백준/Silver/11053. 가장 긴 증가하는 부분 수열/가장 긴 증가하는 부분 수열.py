f2479ec3e6394a4569c16b247d1d3b4615792cb7
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2024-11-24 14:42:47 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 96 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,33 +1,33 @@
 """
 1.전략
-- DP(Dynamic Programming)
-- LIS(Longest Increasing Subsequence) 유형 문제
-- 접근 : A[i]를 마지막 값으로 가지는가장 긴 증가 부분수열의 길이 찾기
-- 점화식 : dp[i] = max(dp[i], dp[j]+1)
+- 다이나믹 프로그래밍(Dynamic Programming)
+- 현재 자릿수(i) 기준으로 증가하는 부분 수열의 최댓값을 누적
+- 점화식: A[i] > A[1~i-1]인 경우 => dp[i] = max(dp[i], dp[j]+1)
 
-2.시간복잡도
-- O(N^2) = 1,000 * 1,000 = 1,000,000(Worst case)
-  => 1초 이내 가능
+2.시간 복잡도
+- 시간 제한: 1초
+- O(N^2) = 1,000 * 1,000 = 1,000,000
 """
-def solution(A: list, N: int):
+import sys
+input = sys.stdin.readline
+
+# Input
+N = int(input())
+A = [0] + list(map(int, input().split()))
+
+# Variables
+dp = [1 for _ in range(N+1)]
+
+# Solution
+def DP(n: int):
     global dp
 
-    for i in range(1, N):
-        for j in range(i):
-            if A[j] < A[i]:
+    for i in range(2, n+1):
+        for j in range(1, i):
+            if A[i] > A[j]:
                 dp[i] = max(dp[i], dp[j]+1)
 
     return max(dp)
 
-# 입력받기
-N = int(input())
-A = list(map(int, input().split()))
-
-# 필요한 변수 선언 및 초기화
-dp = [1] * N
-
-# 함수 실행
-res = solution(A, N)
-
-# 결과 출력
-print(res)
+# Output
+print(DP(N))
