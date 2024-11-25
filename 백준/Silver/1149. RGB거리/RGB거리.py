5c5b21326f020e23ce8e27d0ae31de59fc3ed668
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2024-11-25 16:41:11 +0900

    [Silver I] Title: RGB거리, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,34 +1,38 @@
 """
 1.전략
-- DP(Dynamic Programming)
-- 첫 행을 첫번째 집에 각 색을 칠하는 비용으로 채워넣은 후,
-- 이전 집에서 칠한 색을 제외한 색 중 더 저렴한 색을 칠하면 되는 문제
+- 다이나믹 프로그래밍(Dynamic Programming)
+- 규칙: 이웃 간의 집의 색은 모두 달라야 하며 비용은 최솟값을 가져야 함
+- 선택된 행 기준, 같은 열이 아니며 합이 최소가 되는 값을 현재 인덱스에 저장
+- 점화식
+  => dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + arr[i][0]
+  => dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + arr[i][1]
+  => dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + arr[i][2]
 
-2.시간복잡도
+2.시간 복잡도
+- 시간 제한: 0.5초
 - O(N) = 1,000
-  => 0.5초 이내 가능
 """
-import sys; input = sys.stdin.readline
+import sys
+input = sys.stdin.readline
 
-def DP(N: int):
-  for i in range(1, N):
-    # 현재 열을 제외한 i-1행의 열들 중 최솟 값 + 현재 칸 비용
-    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + arr[i][0]
-    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + arr[i][1]
-    dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + arr[i][2]
-
-  return min(dp[N-1])
-
-# 입력 받기
+# Input
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
 
-# 필요한 변수 선언 및 초기화
-dp = [[0] * 3 for _ in range(N)]
+# Variables
+dp = [[1000 for _ in range(3)] for _ in range(N)]
 dp[0] = arr[0]
 
-# 함수 실행
-res = DP(N)
+def DP(n: int):
+    global dp
 
-# 결과 출력
+    for i in range(1, N):
+        dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + arr[i][0]
+        dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + arr[i][1]
+        dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + arr[i][2]
+
+    return min(dp[n-1])
+
+# Main
+res = DP(N)
 print(res)
