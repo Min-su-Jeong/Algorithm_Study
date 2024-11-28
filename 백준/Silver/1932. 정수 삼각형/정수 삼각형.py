7d791f778518b0257af0fa10af40d7998b4f2c9f
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2024-11-28 23:30:05 +0900

    [Silver I] Title: 정수 삼각형, Time: 88 ms, Memory: 35608 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,36 +1,27 @@
-"""
-1.전략
-- DP(Dynamic Programming)
-- 행의 첫번째 숫자(j = 0)인 경우, 이전 행의 같은 열의 숫자 더하기
-- 행의 마지막 숫자(j = i)인 경우, 이전 행의 마지막 열 숫자 더하기
-- 이외의 경우, 이전 행의 같은 열(j) 또는 자신의 열 -1인 숫자를 비교해서 큰 값 더하기
+import sys
+input = sys.stdin.readline
 
-2.시간복잡도
-- O(N^2) = 500 ^ 2 = 250,000
-  => 2초 이내 가능
-"""
-import sys; input = sys.stdin.readline
-
-def DP(n: int):
-  global dp
-
-  for i in range(1, n):
-    for j in range(i+1):
-      if j == 0:
-        dp[i][j] += dp[i-1][0]
-      elif j == i:
-        dp[i][j] += dp[i-1][j-1]
-      else:
-        dp[i][j] += max(dp[i-1][j-1], dp[i-1][j])
-
-  return max(dp[n-1])
-
-# 입력 받기
+# Input
 n = int(input())
 dp = [list(map(int, input().split())) for _ in range(n)]
 
-# 함수 실행
+# Solution
+def DP(n: int) -> int:
+    global dp
+
+    for i in range(1, n):
+        for j in range(i+1):
+            if j == 0:
+                dp[i][j] += dp[i-1][j]
+            elif j == i:
+                dp[i][j] += dp[i-1][j-1]
+            else:
+                dp[i][j] += max(dp[i-1][j], dp[i-1][j-1])
+
+    return max(dp[n-1])
+
+# Main
 res = DP(n)
 
-# 결과 출력
+# Output
 print(res)
