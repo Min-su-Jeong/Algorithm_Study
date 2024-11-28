c063d3d4dfb2a1727d2dc3e136f72632c944c4e2
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2024-11-28 23:09:32 +0900

    [Silver I] Title: 포도주 시식, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,39 +1,41 @@
 """
 1.전략
-- DP(Dynamic Programming)
-- n-2까지의 최대 양 + 현재 양 = (dp[n-2] + wine[n])
-- n-3까지의 최대 양 + 전(n-1)번째 양 + 현재 양 = (dp[n-3] + wine[n-1] + wine[n])
+- 다이나믹 프로그래밍(Dynamic Programming)
+- 연속 3잔 마시기 불가능 => 케이스 생각 + dp[n]은 n번째까지 최대로 마신 포도주 잔의 개수로 저장
+  1. n-2번째까지 최대로 마신 후 n번째 와인 마시기
+  2. n-3번째까지 최대로 마신 후 n-1, n번째 와인 마시기
+  3. n번째 와인을 안마시는 경우 => n-1번째 값과 최댓값 비교
+- 점화식: dp[i] = max(dp[i-1], dp[i-2] + wine[i],  dp[i-3] + wine[i-1] + wine[i])
 
-2.시간복잡도
+2.시간 복잡도
+- 시간 제한: 2초
 - O(N) = 10,000
-  => 2초 이내 가능
 """
-import sys; input = sys.stdin.readline
+import sys
+input = sys.stdin.readline
 
-def DP(n: int):
-  global dp, wine
-
-  # 1. 현재 포도주를 마시지 않았을 경우
-  # 2. 현재 포도주와 이전 포도주를 마셨을 경우
-  # 3. 마시지 않았을 경우를 비교
-  for i in range(3, n+1): 
-    dp[i] = max(dp[i - 1], wine[i] + wine[i - 1] + dp[i - 3], wine[i] + dp[i - 2])
-
-  return max(dp)
-
-# 입력 받기
+# Input
 n = int(input())
 wine = [0] * 10001
 for i in range(1, n+1):
     wine[i] = int(input())
 
-# 필요한 변수 선언 및 초기화
-dp = [0] * 10001
+# Variables
+dp = [0 for _ in range(10001)]
 dp[1] = wine[1]
 dp[2] = wine[1] + wine[2]
 
-# 함수 실행
+# Solution
+def DP(n: int) -> int:
+    global dp
+
+    for i in range(3, n+1):
+        dp[i] = max(dp[i-1], dp[i-2] + wine[i],  dp[i-3] + wine[i-1] + wine[i])
+
+    return max(dp)
+
+# Main
 res = DP(n)
 
-# 결과 출력
+# Output
 print(res)
