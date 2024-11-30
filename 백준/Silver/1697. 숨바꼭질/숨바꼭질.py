300d1aca2e73cba3ad8975105364870ccb7ff31b
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2024-11-30 14:47:36 +0900

    [Silver I] Title: 숨바꼭질, Time: 92 ms, Memory: 35288 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,18 +1,40 @@
-import sys
+"""
+1.전략
+- BFS(Breadth-First Search)
+- 덱(Deque)을 사용해서 수빈이의 위치를 기준으로 X-1, X+1, 2*X의 위치를 순회
+
+2.시간 복잡도
+- 시간 제한: 2초
+- O(N) = 100,000 * 3 = 300,000
+"""
 from collections import deque
+import sys
+input = sys.stdin.readline
 
-def bfs(v):
-    q = deque([v])
-    while q:
-        v = q.popleft()
-        if v == k:
-            return arr[v]
-        for nv in (v-1, v+1, v*2):
-            if 0 <= nv < MAX and not arr[nv]:
-                arr[nv] = arr[v] + 1
-                q.append(nv)
+# Input
+N, K = map(int, input().split())
 
+# Variables
 MAX = 100001
-arr = [0] * MAX
-n, k = map(int, sys.stdin.readline().split())
-print(bfs(n))
+arr = [0 for _ in range(MAX)]
+
+# Solution
+def bfs(x: int):
+    q = deque([x])
+
+    while q:
+        x = q.popleft()
+        
+        if x == K:
+            return arr[x]
+        
+        for nx in (x-1, x+1, 2*x):
+            if 0 <= nx < MAX and not arr[nx]:
+                arr[nx] = arr[x] + 1
+                q.append(nx)
+
+# Main
+res = bfs(N)
+
+# Output
+print(res)
