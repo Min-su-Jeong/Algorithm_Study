db248523e3503c03a88c74b3983f76f8dec71385
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2024-12-07 19:42:51 +0900

    [Silver II] Title: DFS와 BFS, Time: 68 ms, Memory: 35684 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,36 +1,57 @@
-import sys
-from collections import deque
+"""
+1.전략
+- DFS, BFS
+- 인접 리스트 사용(sort 추가 필요)
+- 정점(N), 간선(M), 시작점(V)이 주어질 때 방문 순회 원리 이해하기
 
-def dfs(v):
-    visited1[v] = 1
-    print(v, end = " ")
-    for i in range(1, n + 1):
-        if visited1[i] == 0 and graph[v][i] == 1:
-            dfs(i)
-            
-def bfs(v):
+2.시간 복잡도
+- 시간 제한: 2초
+- O(N+M) = 1,000 + 10,000 = 11,000
+"""
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+# Input
+N, M, V = map(int, input().split())
+
+# Variables
+graph = {i:[] for i in range(N+1)}
+visited1 = [False for _ in range(N+1)]
+visited2 = [False for _ in range(N+1)]
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for k in graph:
+    graph[k].sort()
+
+# Solution
+def DFS(v: int):
+    print(v, end=' ')
+    visited1[v] = True
+    
+    for i in graph[v]:
+        if not visited1[i]:
+            DFS(i)
+
+def BFS(v: int):
     q = deque()
     q.append(v)
-    visited2[v] = 1
+    visited2[v] = True
+
     while q:
-        v = q.popleft()
-        print(v, end = " ")
-        for i in range(1, n + 1):
-            if visited2[i] == 0 and graph[v][i] == 1:
+        x = q.popleft()
+        print(x, end=' ')
+
+        for i in graph[x]:
+            if not visited2[i]:
+                visited2[i] = True
                 q.append(i)
-                visited2[i] = 1
 
-input = sys.stdin.readline
-n, m, v = map(int, input().split())
-
-graph = [[0] * (n + 1) for _ in range(n + 1)]
-visited1 = [0] * (n + 1)
-visited2 = [0] * (n + 1)
-
-for _ in range(m):
-    a, b = map(int, input().split())
-    graph[a][b] = graph[b][a] = 1
-
-dfs(v)
+# Main
+DFS(V)
 print()
-bfs(v)
+BFS(V)
