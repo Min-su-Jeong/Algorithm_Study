1ffb6cf3e44e709fa2fef486873974e78d2e4341
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-01-23 18:35:18 +0900

    [Silver V] Title: 단어 정렬, Time: 80 ms, Memory: 37024 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,12 +1,24 @@
-n = int(input())
-lst = []
+"""
+1.전략
+- 구현
+- 중복 제거: set() 함수 사용
+- 조건: list의 sort() 함수 사용
 
-for i in range(n):
-    lst.append(input())
-    
-lst = list(set(lst))
-lst.sort()
-lst.sort(key=len)
+2.시간 복잡도
+- 시간 제한: 2초
+- 시간 복잡도: O(NlogN)
+"""
+import sys
+input = sys.stdin.readline
 
-for word in lst:
-    print(word)
+# Input
+N = int(input())
+words = set(input().rstrip() for _ in range(N))
+
+# Solution
+words = list(words)
+words.sort(key=lambda x: (len(x), x))
+
+# Output
+for w in words:
+    print(w)
