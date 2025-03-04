cc269cbf576dc36e1eb46160bc277d2ba7ce66ea
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-03-04 17:50:16 +0900

    [Silver II] Title: 사탕 게임, Time: 1184 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.py
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -1,16 +1,3 @@
-"""
-1. 전략
-- 브루트포스(Brute Force)
-- N의 최대 크기(50)가 크지 않음. 여러 개의 for문 사용 가능하다고 판단됨.
-- 2차원 리스트(board)를 만들어서 현재 위치 기준 오른쪽, 아래 Swap
-- Swap한 이후, 행과 열 기준으로 사탕의 최대 개수 구하기
-- 개수를 구한 후, 배열 원상 복구
-
-2. 시간 복잡도
-- 시간 제한: 1초
-- O(N^4) = 50^4 = 6,250,000 (가능)
-"""
-
 import sys
 input = sys.stdin.readline
 
@@ -18,48 +5,45 @@ input = sys.stdin.readline
 N = int(input())
 board = [list(map(str, input().rstrip())) for _ in range(N)]
 
-# Solution
+# Variables
 res = 0
 
+# Solution
 def findMax(board: list):
     global res
 
     for i in range(N):
-        # 행 탐색
-        cntRow = 1
+        maxRow = 1
         for j in range(N-1):
             if board[i][j] == board[i][j+1]:
-                cntRow += 1
-                res = max(res, cntRow)
+                maxRow += 1
+                res = max(res, maxRow)
             else:
-                cntRow = 1
-
-        # 열 탐색
-        cntCol = 1
+                maxRow = 1
+                
+        maxCol = 1
         for j in range(N-1):
             if board[j][i] == board[j+1][i]:
-                cntCol += 1
-                res = max(res, cntCol)
+                maxCol += 1
+                res = max(res, maxCol)
             else:
-                cntCol = 1
+                maxCol = 1
 
-for i in range(N):
-    for j in range(N-1):
-        if board[i][j] != board[i][j+1]:
-            # 오른쪽 Swap
-            board[i][j], board[i][j+1] = board[i][j+1], board[i][j]
-            # 최대 개수 구하기
-            findMax(board)
-            # 원복
-            board[i][j], board[i][j+1] = board[i][j+1], board[i][j]
+def solution():
+    for i in range(N):
+        for j in range(N-1):
+            if board[i][j] != board[i][j+1]:
+                board[i][j], board[i][j+1] = board[i][j+1], board[i][j]
+                findMax(board)
+                board[i][j], board[i][j+1] = board[i][j+1], board[i][j]
 
-        if board[j][i] != board[j+1][i]:
-            # 아래쪽 Swap
-            board[j][i], board[j+1][i] = board[j+1][i], board[j][i]
-            # 최대 개수 구하기
-            findMax(board)
-            # 원복
-            board[j][i], board[j+1][i] = board[j+1][i], board[j][i]
+            if board[j][i] != board[j+1][i]:
+                board[j][i], board[j+1][i] = board[j+1][i], board[j][i]
+                findMax(board)
+                board[j][i], board[j+1][i] = board[j+1][i], board[j][i]
+
+# Main
+solution()
 
 # Output
 print(res)
