411e9d451165c2511b994e6fa3fc56573753bf71
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-03-05 16:07:59 +0900

    [Gold V] Title: 리모컨, Time: 240 ms, Memory: 110760 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.py
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -1,31 +1,35 @@
-"""
-1. 전략
-- 브루트포스(Brute Force)
-- 채널이 아래에서부터 올라가는 경우, 위에서 내려가는 경우를 고려
-- 현재 채널의 숫자를 str로 변환해서 고장난 버튼에 들어있는지 확인
-- 현재 채널에 고장난 버튼이 속하지 않으면 버튼을 누르는 횟수 계산
-- "최적해 = min(마지막 최솟 값, 숫자 버튼 클릭 수 + '+/-' 버튼 클릭 수)"
-
-2. 시간 복잡도
-- 시간 제한: 2초
-- O(N*M) = 1,000,000 * 7 = 7,000,000
-"""
-
-import sys;
+import sys
 input = sys.stdin.readline
 
+# Input
 N = int(input())
 M = int(input())
-broken = list(input().split()) if M != 0 else []
+if M:
+    broken = set(input().split())
+else:
+    broken = set()
 
-MAX = 1000001
-res = abs(100 - N)
+if N == 100:
+    print(0)
+    sys.exit(0)
+    
+# Variables
+res = abs(100-N)
+MAX = 1000000
 
-for i in range(MAX):
-    for j in str(i):
-        if j in broken:
-            break
-    else:
-        res = min(res, len(str(i)) + abs(i - N))
+# Solution
+def solution():
+    global res
 
+    for i in range(MAX):
+        for j in str(i):
+            if j in broken:
+                break
+        else:
+            res = min(res, len(str(i)) + abs(i-N))
+
+# Main
+solution()
+
+# Output
 print(res)
