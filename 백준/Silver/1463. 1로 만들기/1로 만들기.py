1fb080df0185dd2ec2e1511e364237be12357327
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-03-18 00:16:52 +0900

    [Silver III] Title: 1로 만들기, Time: 116 ms, Memory: 118552 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,12 +1,3 @@
-"""
-- 다이나믹 프로그래밍(Dynamic Programming)
-- N이 2 또는 3으로 나누어 떨어지는 경우와 둘 다 나누어 떨어지지 않아 -1을 하는 경우로 나눌 수 있음
-- -1을 먼저 했을 떄의 결과값과 나누어 떨어지는 경우에 나누었을 떄의 결과 값을 비교해서 최솟값 찾기
-
-2.시간 복잡도
-- 시간 제한: 0.7초
-- O(N) = 10^6 = 1,000,000
-"""
 import sys
 input = sys.stdin.readline
 
@@ -14,17 +5,23 @@ input = sys.stdin.readline
 N = int(input())
 
 # Variables
-res = 0
 dp = [0 for _ in range(N+1)]
 
 # Solution
-for i in range(2, N+1):
-    dp[i] = dp[i-1] + 1
+def solution():
+    global dp
 
-    if i % 2 == 0:
-        dp[i] = min(dp[i], dp[i//2]+1)
-    if i % 3 == 0:
-        dp[i] = min(dp[i], dp[i//3]+1)
+    for i in range(2, N+1):
+        dp[i] = dp[i-1] + 1
+
+        if i % 2 == 0:
+            dp[i] = min(dp[i], dp[i//2] + 1)
+
+        if i % 3 == 0:
+            dp[i] = min(dp[i], dp[i//3] + 1)
     
+# Main
+solution()
+
 # Output
 print(dp[N])
