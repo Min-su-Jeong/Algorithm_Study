5a7d3ff343e2ea5aa0bd3dcbe2a618dcb4570812
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-03-22 23:33:57 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 100 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.py
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.py
@@ -1,35 +1,26 @@
-"""
-1.전략
-- 다이나믹 프로그래밍(Dynamic programming)
-- i: 구하고 싶은 카드의 장수
-- j: i보다 작은 카드의 장수
-- j와 미지수 x의 합이 i가 나와야하기 때문에 
-- j + x = i 라는 식을 만족시켜야 함 => x = i - j (i > j)
-
-2.시간복잡도
-- O(N^2) = 1,000 * 1,000 = 1,000,000
-  => 1초 이내 가능
-"""
 import sys
 input = sys.stdin.readline
 
 # Input
 N = int(input())
-cost = [0] + list(map(int, input().split()))
+P = list(map(int, input().split()))
 
 # Variables
 dp = [10000 for _ in range(N+1)]
 dp[0] = 0
 
 # Solution
-def DP(n) -> int:
+def solution(n: int):
     global dp
 
     for i in range(1, n+1):
         for j in range(1, i+1):
-            dp[i] = min(dp[i], dp[i-j] + cost[j])
+            dp[i] = min(dp[i], dp[i-j]+P[j-1])
 
     return dp[n]
 
+# Main
+res = solution(N)
+
 # Output
-print(DP(N))
+print(res)
