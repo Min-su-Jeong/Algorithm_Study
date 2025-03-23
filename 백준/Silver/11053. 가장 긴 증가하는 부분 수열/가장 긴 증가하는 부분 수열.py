0d5ea7899ab217240cfb5a9b74e64bb21efcb42e
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-03-23 20:25:33 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 76 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,13 +1,3 @@
-"""
-1.전략
-- 다이나믹 프로그래밍(Dynamic Programming)
-- 현재 자릿수(i) 기준으로 증가하는 부분 수열의 최댓값을 누적
-- 점화식: A[i] > A[1~i-1]인 경우 => dp[i] = max(dp[i], dp[j]+1)
-
-2.시간 복잡도
-- 시간 제한: 1초
-- O(N^2) = 1,000 * 1,000 = 1,000,000
-"""
 import sys
 input = sys.stdin.readline
 
@@ -19,7 +9,7 @@ A = [0] + list(map(int, input().split()))
 dp = [1 for _ in range(N+1)]
 
 # Solution
-def DP(n: int):
+def solution(n: int):
     global dp
 
     for i in range(2, n+1):
@@ -29,5 +19,8 @@ def DP(n: int):
 
     return max(dp)
 
+# Main
+res = solution(N)
+
 # Output
-print(DP(N))
+print(res)
