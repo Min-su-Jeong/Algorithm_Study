15725a811ce6b8dd0ea3626d5985f2a05964837e
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-04-02 10:04:31 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 112 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
@@ -1,45 +1,32 @@
-"""
-1.전략
-- DP(Dynamic Programming)
-- i번째 수 기준으로 이전 수들의 증감 여부를 판단
-- 증가하는 구간: dp1[i] + 1 / 감소하는 구간: dp2[i] + 1
-- dp1과 dp2의 합을 이용해 바이토닉 수열의 조건에 맞는 길이 구하기
+import sys
+input = sys.stdin.readline
 
-2. 시간복잡도
-- O(N^2) = 1,000 ^ 2 = 1,000,000 (Worst case)
-  => 1초 이내 가능
-"""
-import sys; input = sys.stdin.readline
+# Input
+N = int(input())
+A = [0] + list(map(int, input().split()))
 
-def DP(N: int, A: list):
-    global dp1, dp2
-    
-    for i in range(1, N):
+# Variables
+dp1 = [0 for _ in range(N+1)]
+dp2 = [0 for _ in range(N+1)]
+
+# Solution
+def solution(n: int):
+    global dp
+
+    for i in range(1, n+1):
         for j in range(i):
-            if A[j] < A[i]:
+            if A[i] > A[j]:
                 dp1[i] = max(dp1[i], dp1[j]+1)
-    
-    A.reverse()
-    
-    for i in range(1, N):
-        for j in range(i):
-            if A[j] < A[i]:
+
+    for i in range(n-1, 0, -1):
+        for j in range(n, i, -1):
+            if A[i] > A[j]:
                 dp2[i] = max(dp2[i], dp2[j]+1)
 
-    res = [a+b for a, b in zip(dp1, dp2[::-1])]
+    return max([x + y for x, y in zip(dp1,dp2)])
 
-    return max(res)-1
+# Main
+res = solution(N)
 
-# 입력 받기
-N = int(input())
-A = list(map(int, input().split()))
-
-# 필요한 변수 생성 및 초기화
-dp1 = [1] * N
-dp2 = [1] * N
-
-# 함수 실행
-res = DP(N, A)
-
-# 결과 출력
+# Output
 print(res)
