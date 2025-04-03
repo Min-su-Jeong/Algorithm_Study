4766bfb52cfed8e4619a0d0e835aa40e01c41dab
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-04-03 10:17:26 +0900

    [Gold IV] Title: 타일 채우기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2133. 타일 채우기/타일 채우기.py
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.py
@@ -1,39 +1,25 @@
-"""
-1.전략
-- DP(Dynamic Programming)
-- 1) dp[N-2] 값에 가로 길이 2짜리 타일로 만들수 있는 경우
-  -> dp[N-2] * 3
-- 2) 0~N-4까지의 타일 뒤에 자신을 붙혀서 만들 수 있는 경우
-  -> (dp[0] + dp[2] + ... dp[N-4]) * 2
-- 3) 가로 길이가 N인 새로운 타일 덩어리를 만드는 경우
-  -> 2
-- 3가지 경우의 합을 구함 => dp[N]
-- 점화식 : dp[N] = dp[N-2] * 3 + sum(dp[:N-2]) * 2 + 2
+import sys
+input = sys.stdin.readline
 
-2.시간복잡도
-- O(N^2) = 30 * 30 = 900 (Worst case)
-"""
-def DP(N: int):
-    global dp
-
-    for i in range(4, N+1):
-        if i % 2 == 0:
-            dp[i] = dp[i-2] * 3 + sum(dp[:i-2]) * 2 + 2
-        else:
-            dp[i] = 0
-
-    return dp[N]
-
-# 입력 받기
+# Input
 N = int(input())
 
-# 필요한 변수 선언 및 초기화
+# Variables
 MAX = 31
-dp = [0] * MAX
+dp = [0 for _ in range(MAX)]
 dp[2] = 3
 
-# 함수 실행
-res = DP(N)
+# Solution
+def solution(n: int):
+    global dp
 
-# 결과 출력
+    for i in range(4, n+1, 2):
+        dp[i] = dp[i-2]*3 + sum(dp[:i-2])*2 + 2
+
+    return dp[n]
+
+# Main
+res = solution(N)
+
+# Output
 print(res)
