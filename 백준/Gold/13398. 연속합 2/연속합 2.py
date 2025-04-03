3e8e491ba6adc7a69a29b4e0d04bda84e778d122
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-04-03 09:49:28 +0900

    [Gold V] Title: 연속합 2, Time: 108 ms, Memory: 42660 KB -BaekjoonHub

--- 백준/Gold/13398. 연속합 2/연속합 2.py
+++ 백준/Gold/13398. 연속합 2/연속합 2.py
@@ -1,36 +1,27 @@
-"""
-1.전략
-- DP(Dynamic Programming)
-- dp[0][i]: 수를 제거하지 않고 구하는 연속합
-- dp[1][i]: 수를 제거하고 구하는 연속합
-- 점화식: dp[1][i] = max(dp[0][i-1], dp[1][i-1] + a[i])
+import sys
+input = sys.stdin.readline
 
-2. 시간복잡도
-- O(N) = 100,000(Worst case)
-  => 2초 이내 가능
-"""
-import sys; input = sys.stdin.readline
+# Input
+n = int(input())
+arr = list(map(int, input().split()))
 
-def DP(n: int, arr: list):
-    global dp
+# Variables
+dp = [[-1000 for _ in range(n)] for _ in range(2)]
+res = -1000
 
-    res = -1000
+# Solution
+def solution():
+    global dp, res
+
     for i in range(n):
-        dp[0][i] = max(arr[i], dp[0][i-1] + arr[i])
+        dp[0][i] = max(arr[i], dp[0][i-1]+arr[i])
         dp[1][i] = max(dp[0][i-1], dp[1][i-1] + arr[i])
         res = max(res, dp[0][i], dp[1][i])
 
     return res
 
-# 입력 받기
-n = int(input())
-arr = list(map(int,input().split()))
+# Main
+res = solution()
 
-# 필요한 변수 생성 및 초기화
-dp = [[-1000]*n for _ in range(2)]
-
-# 함수 실행
-res = DP(n, arr)
-
-# 결과 출력
+# Output
 print(res)
