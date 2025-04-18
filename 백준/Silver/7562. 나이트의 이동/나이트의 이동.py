674e1de9127693c9f71d98cfa1308f2200a16410
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-04-18 09:41:34 +0900

    [Silver I] Title: 나이트의 이동, Time: 1228 ms, Memory: 35004 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -1,58 +1,51 @@
-"""
-1.전략
-- BFS
-- BFS 알고리즘을 사용하여 8방향으로 탐색
-
-2.시간복잡도
-- O(N^2) = 300 * 300 = 90,000 (Worst case)
-"""
-import sys; input = sys.stdin.readline
+import sys
 from collections import deque
+input = sys.stdin.readline
 
-def bfs():
-        q = deque()
+# Input
+N = int(input())
 
-        q.append(now)
-        visited[now[0]][now[1]]
+# Variables
+dx = [-2, -2, -1, -1, 1, 1, 2, 2]
+dy = [-1, 1, -2, 2, -2, 2, -1, 1]
 
-        while q:
-            x, y = q.popleft()
+# Solution
+def bfs(start, end, board, l):
+    q = deque()
+    q.append(start)
+    visited[start[0]][start[1]] = True
 
-            if x == dest[0] and y == dest[1]:
-                return 0
+    while q:
+        x, y = q.popleft()
 
-            for i in range(8):
-                nx = x + dx[i]
-                ny = y + dy[i]
+        if x == end[0] and y == end[1]:
+            return 0
+        
+        for i in range(8):
+            nx = x + dx[i]
+            ny = y + dy[i]
 
-                if nx < 0 or nx >= n or ny < 0 or ny >= n:
-                    continue
+            if (nx < 0 or nx >= l) or (ny < 0 or ny >= l):
+                continue
+            
+            if nx == end[0] and ny == end[1]:
+                visited[nx][ny] = True
+                return board[x][y] + 1
+            
+            if not visited[nx][ny]:
+                q.append((nx, ny))
+                visited[nx][ny] = True
+                board[nx][ny] = board[x][y] + 1
 
-                if nx == dest[0] and ny == dest[1]:
-                    visited[nx][ny] = True
-                    return graph[x][y]+1
+# Main
+for _ in range(N):
+    l = int(input())
+    start = tuple(map(int, input().split()))
+    end = tuple(map(int, input().split()))
 
-                if visited[nx][ny] == False:
-                    q.append([nx,ny])
-                    visited[nx][ny] = True
-                    graph[nx][ny] = graph[x][y] + 1
-
-# 입력 받기             
-t = int(input()) 
-
-# 필요한 변수 생성 및 초기화
-dx = [-2, -1, 1, 2, 2, 1, -1, -2]
-dy = [1, 2, 2, 1, -1, -2, -2, -1]
-
-for _ in range(t):
-    # 입력 받기
-    n = int(input())
-    now = list(map(int, input().split()))
-    dest = list(map(int, input().split()))   
+    board = [[0 for _ in range(l)] for _ in range(l)]
+    visited = [[False for _ in range(l)] for _ in range(l)]
     
-    # 필요한 변수 생성 및 초기화 
-    graph = [[0] * n for _ in range(n)]
-    visited = [[False] * n for _ in range(n)]
-    
-    # 함수 실행 및 결과 출력
-    print(bfs())
+    # Output
+    res = bfs(start, end, board, l)
+    print(res)
