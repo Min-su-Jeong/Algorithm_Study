01a1e88bc6d0cf105412d8e0aeb14d566c0af485
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-05-04 00:46:34 +0900

    [Silver IV] Title: 주몽, Time: 76 ms, Memory: 2080 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.cc
+++ 백준/Silver/1940. 주몽/주몽.cc
@@ -3,21 +3,19 @@ using namespace std;
 
 int N, M, ret, a[15004];
 
-void solve() {
-    for (int i=0; i<N; i++) {
-        for (int j=0; j<i; j++) {
-            if (a[i]+a[j] == M) ret++;
-        }
-    }
-}
-
 int main() {
-    cin >> N;
-    cin >> M;
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    cin >> N >> M;
     for (int i=0; i<N; i++) cin >> a[i];
 
-    solve();
-    
-    cout << ret << '\n';
+    if (M > 200000) cout << 0 << "\n";
+    else {
+        for (int i=0; i<N; i++) {
+            for (int j=i+1; j<N; j++) {
+                if (a[i] + a[j] == M) ret++;
+            }
+        }
+        cout << ret << "\n";
+    }
     return 0;
 }
