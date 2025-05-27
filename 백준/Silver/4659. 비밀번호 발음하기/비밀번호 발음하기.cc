6ffb87a6a774b9d0f9c08d09c89e3184d4b7d176
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-05-27 22:43:29 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string s;
+int vcnt, ccnt; // 모음, 자음 개수
+
+bool isVowel(int idx) {
+    return (idx == 'a' || idx == 'e' || idx == 'i' || idx == 'o' || idx == 'u');
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    while(true) {
+        cin >> s;
+        if (s == "end") break;
+        
+        vcnt = ccnt = 0;       // 모음, 자음 개수 초기화
+        bool flag = 0;         // 가능 여부 검사
+        bool is_include_v = 0; // 모음 포함 여부 검사
+        int prev = -1;         // 이전 값 검사
+
+        for (int i=0; i<s.size(); i++) {
+            // 조건 1
+            int idx = s[i];
+            if (isVowel(idx)) vcnt++, ccnt = 0, is_include_v = 1;
+            else vcnt = 0, ccnt++;
+
+            // 조건 2
+            if (vcnt == 3 || ccnt == 3) flag = 1;
+
+            // 조건 3
+            if (i >= 1 && (prev == idx) && (idx != 'e' && idx != 'o')) flag = 1;
+            prev = idx;
+        }
+
+        if (!is_include_v) flag = 1;
+        if (flag) cout << "<" << s << ">" << " is not acceptable.\n";
+        else cout << "<" << s << ">" << " is acceptable.\n";
+    }
+    return 0;
+}
