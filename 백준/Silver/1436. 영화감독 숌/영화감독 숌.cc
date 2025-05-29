5f7c45abac815dc6b88269ebd31f95ad711580bf
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-05-29 22:09:55 +0900

    [Silver V] Title: 영화감독 숌, Time: 68 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    cin >> N;
+    int ret = 666;
+    for(;; ret++) {
+        if (to_string(ret).find("666") != string::npos) N--;
+        if (N == 0) break;
+    }
+    cout << ret << '\n';
+    
+    return 0;
+}
