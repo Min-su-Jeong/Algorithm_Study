b677a324e38c6373df585cfe85d1a4c1411e174b
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-05-30 20:03:09 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
@@ -5,6 +5,8 @@ int N;
 string init_s, s, pre, suf;
 
 int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
     cin >> N;
     cin >> init_s;
     int pos = init_s.find('*');
@@ -16,11 +18,10 @@ int main() {
         
         // 반례
         if (pre.size() + suf.size() > s.size()) {
-            cout << "NE" << "\n";
+            cout << "NE\n";
         } else {
-            // 메인 로직
-            if (pre == s.substr(0, pre.size()) && suf == s.substr(s.size() - suf.size())) cout << "DA" << "\n";
-            else cout << "NE" << "\n";
+            if (pre == s.substr(0, pre.size()) && suf == s.substr(s.size()-suf.size())) cout << "DA\n";
+            else cout << "NE\n";
         }
     }
     return 0;
