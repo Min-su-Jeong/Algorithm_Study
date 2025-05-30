7a766fb459c702a9d43fe7075c3a92a00ada91fa
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-05-30 19:40:27 +0900

    [Bronze I] Title: ROT13, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.cc
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -1,24 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string s;
+string s, ret;
 
 int main() {
     getline(cin, s);
 
-    for (int i=0; i < s.size(); i++) {
-        // 대문자일 경우
+    for (int i=0; i<s.size(); i++) {
+        // 대문자인 경우
         if (s[i] >= 65 && s[i] <= 90) {
             if (s[i] + 13 > 90) s[i] = s[i] + 13 - 26;
-            else s[i] = s[i] + 13;
+            else s[i] += 13;
         }
-        // 소문자일 경우
-        else if (s[i] >= 97 && s[i] <= 122) {
-            if (s[i] + 13 > 122) s[i] = s[i] + 13 - 26;
-            else s[i] = s[i] + 13;
-        }
-        cout << s[i];
-    }
 
+        // 소문자인 경우
+        if (s[i] >= 97 && s[i] <= 122) {
+            if (s[i] + 13 > 122) s[i] = s[i] + 13 - 26;
+            else s[i] += 13;
+        }
+    }
+    cout << s << '\n';
     return 0;
 }
