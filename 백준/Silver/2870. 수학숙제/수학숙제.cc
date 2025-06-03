70f9eb73e4465dab1abe93659436ded24e63c185
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-06-03 21:03:30 +0900

    [Silver IV] Title: 수학숙제, Time: 0 ms, Memory: 2168 KB -BaekjoonHub

--- 백준/Silver/2870. 수학숙제/수학숙제.cc
+++ 백준/Silver/2870. 수학숙제/수학숙제.cc
@@ -17,24 +17,25 @@ void go() {
 
 bool cmp(string a, string b) {
     if (a.size() == b.size()) return a < b;
-    else return a.size() < b.size();
+    return a.size() < b.size();
 }
 
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
     cin >> N;
-    for (int i=0; i<N; i++) {
+    while (N--) {
         cin >> s;
         ret = "";
-        for (int j=0; j<s.size(); j++) {
-            int code = s[j];
-            if (code < 65) ret += s[j];
+        for (int i=0; i<s.size(); i++) {
+            int idx = s[i];
+            if (idx < 65) ret += s[i];
             else if (ret.size()) go();
         }
         if (ret.size()) go();
     }
     sort(v.begin(), v.end(), cmp);
     for (string i: v) cout << i << '\n';
+    
     return 0;
 }
