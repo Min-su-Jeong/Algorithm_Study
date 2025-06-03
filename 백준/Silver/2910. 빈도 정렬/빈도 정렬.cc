1b2c49b702e0ca00d03099b128c380ecbda80cb4
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-06-03 17:30:23 +0900

    [Silver III] Title: 빈도 정렬, Time: 0 ms, Memory: 2164 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
@@ -3,8 +3,8 @@ using namespace std;
 
 const int MAX = 1004;
 int N, C, a[MAX];
-vector<pair<int, int>> v;
 map<int, int> mp, mp_first;
+vector<pair<int, int>> v;
 
 bool cmp(pair<int, int> a, pair<int, int> b) {
     if (a.first == b.first) {
@@ -19,18 +19,14 @@ int main() {
     cin >> N >> C;
     for (int i=0; i<N; i++) {
         cin >> a[i]; mp[a[i]]++;
-        if (mp_first[a[i]] == 0) mp_first[a[i]] = i + 1;
+        if (mp_first[a[i]] == 0) mp_first[a[i]] = i+1;
     }
 
-    for (auto it: mp) {
-        v.push_back({it.second, it.first});
-    }
-
+    for (auto it: mp) v.push_back({it.second, it.first});
     sort(v.begin(), v.end(), cmp);
-    for (auto i: v) {
-        for (int j=0; j<i.first; j++) {
-            cout << i.second << " ";
-        }
+
+    for (auto it: v) {
+        while(it.first--) cout << it.second << ' ';
     }
     return 0;
 }
