e987315d05d0e7daf538b9c64296b9f5ebe47023
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-06-03 11:47:02 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2104 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,39 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAX = 104;
-int dx[4] = {-1, 1, 0, 0};
-int dy[4] = {0, 0, -1, 1};
-int N, M, graph[MAX][MAX], visited[MAX][MAX], x, y;
+const int MAX = 101;
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+int N, M, x, y, visited[MAX][MAX], graph[MAX][MAX];
 
 int main() {
-    // Input
-    scanf("%d %d", &N, &M);
+    cin >> N >> M;
     for (int i=0; i<N; i++) {
         for (int j=0; j<M; j++) {
             scanf("%1d", &graph[i][j]);
         }
     }
-
-    // BFS
+    
     queue<pair<int, int>> q;
-    visited[0][0] = 1;
     q.push({0, 0});
+    visited[0][0] = 1;
 
-    while(q.size()) {
+    while (q.size()) {
         tie(y, x) = q.front(); q.pop();
-        for (int i=0; i<4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
 
-            if (ny < 0 || ny >= N || nx < 0 || nx >= M || graph[ny][nx] == 0) continue;
-            if (visited[ny][nx]) continue;
+        for (int i=0; i<4; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+
+            if (ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
+            if (graph[ny][nx] == 0 || visited[ny][nx]) continue;
             visited[ny][nx] = visited[y][x] + 1;
             q.push({ny, nx});
-
         }
     }
     printf("%d", visited[N-1][M-1]);
-    
+
     return 0;
 }
