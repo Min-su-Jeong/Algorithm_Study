0c391b37980e1e72fb7134b85d103ff3fa9bb71d
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-06-03 12:12:04 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -1,35 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAX = 54;
-int dx[4] = {-1, 1, 0, 0};
-int dy[4] = {0, 0, -1, 1};
-int M, N, K, X, Y, T, nx, ny, ret;
-int graph[MAX][MAX];
+const int MAX = 51;
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+int T, M, N, K, X, Y, ret, graph[MAX][MAX];
 bool visited[MAX][MAX];
 
 void dfs(int y, int x) {
-    visited[y][x] = true;
+    visited[y][x] = 1;
+    
     for (int i=0; i<4; i++) {
-        nx = x + dx[i];
-        ny = y + dy[i];
-        if (ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
-        if (graph[ny][nx] == 1 && !visited[ny][nx]) {
-            dfs(ny, nx);
-        }
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if (ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
+        if (graph[ny][nx] == 1 && !visited[ny][nx]) dfs(ny, nx);
     }
     return;
 }
 
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin >> T;
 
-    while(T--) {
+    cin >> T;
+    while (T--) {
         ret = 0;
         fill(&graph[0][0], &graph[0][0] + MAX * MAX, 0);
         fill(&visited[0][0], &visited[0][0] + MAX * MAX, 0);
-        
+
         cin >> M >> N >> K;
         for (int i=0; i<K; i++) {
             cin >> X >> Y;
