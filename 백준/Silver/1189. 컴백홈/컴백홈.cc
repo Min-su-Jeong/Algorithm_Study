2ac4af32333febd6143fa697523111fdcb6c4578
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-06-16 23:47:06 +0900

    [Silver I] Title: 컴백홈, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1189. 컴백홈/컴백홈.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAX = 10;
+const int dx[4] = {0, -1, 0, 1};
+const int dy[4] = {1, 0, -1, 0};
+int R, C, K, graph[MAX][MAX], visited[MAX][MAX];
+string s;
+
+int go(int y, int x) {
+    if (y == 0 && x == C-1) {
+        if (visited[y][x] == K) return 1;
+        else return 0;
+    }
+    int ret = 0;
+    for (int i=0; i<4; i++) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if (ny < 0 || nx < 0 || ny >= R || nx >= C) continue;
+        if (visited[ny][nx] || graph[ny][nx] == 'T') continue;
+        visited[ny][nx] = visited[y][x] + 1;
+        ret += go(ny, nx);
+        visited[ny][nx] = 0;
+    }
+    return ret;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> R >> C >> K;
+    for (int i=0; i<R; i++) {
+        cin >> s;
+        for (int j=0; j<C; j++) {
+            graph[i][j] = s[j];
+        }
+    }
+
+    visited[R-1][0] = 1;
+    cout << go(R-1, 0) << '\n';
+
+    return 0;
+}
