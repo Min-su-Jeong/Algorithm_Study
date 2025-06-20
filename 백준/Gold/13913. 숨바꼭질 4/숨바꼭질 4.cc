43ff0d13ced8e3a20410824fa6919d04f75d8c0b
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-06-20 09:14:23 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 8 ms, Memory: 4484 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.cc
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.cc
@@ -7,40 +7,42 @@ int N, K, ret, visited[MAX], prev[MAX];
 vector<int> v;
 queue<int> q;
 
+void bfs() {
+    visited[N] = 1;
+    q.push(N);
+
+    while (q.size()) {
+        int x = q.front();
+        q.pop();
+        
+        if (x == K) {
+            ret = visited[x] - 1;
+            break;
+        }
+        for (int nx: {x+1, x-1, x*2}) {
+            if (nx < 0 || nx >= MAX || visited[nx]) continue;
+            visited[nx] = visited[x] + 1;
+            q.push(nx);
+            prev[nx] = x;
+        }
+    }
+}
+
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
 
     cin >> N >> K;
     if (N == K) {
         cout << 0 << '\n' << N << '\n';
         return 0;
     }
-    
-    visited[N] = 1;
-    q.push(N);
-    
-    while(q.size()) {
-        int x = q.front(); q.pop();
-        if (x == K) {
-            ret = visited[K] - 1;
-            break;
-        }
-        for (int nx: {x+1, x-1, x*2}) {
-            if (nx >= MAX  || nx < 0 || visited[nx]) continue;
-            visited[nx] = visited[x] + 1;
-            prev[nx] = x;
-            q.push(nx);
-        }
-    }
-    
-    for (int i=K; i != N; i=prev[i]) {
-        v.push_back(i);
-    }
+    bfs();
+
+    for (int i=K; i != N; i=prev[i]) v.push_back(i);
     v.push_back(N);
     reverse(v.begin(), v.end());
 
     cout << ret << '\n';
-    for (int i:v) cout << i << ' ';
-
+    for (int i: v) cout << i << ' ';
     return 0;
 }
