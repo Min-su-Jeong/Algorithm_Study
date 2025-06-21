a95b007d10460879b54c7dc0dbbb1c56ab9a989c
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-06-21 11:25:06 +0900

    [Platinum V] Title: 백조의 호수, Time: 108 ms, Memory: 16776 KB -BaekjoonHub

--- 백준/Platinum/3197. 백조의 호수/백조의 호수.cc
+++ 백준/Platinum/3197. 백조의 호수/백조의 호수.cc
@@ -1,80 +1,83 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAX = 1501;
-const int dx[4] = {1, 0, -1, 0};
-const int dy[4] = {0, -1, 0, 1};
-int R, C, ret, y, x, swanY, swanX, visitedSwan[MAX][MAX], visitedWater[MAX][MAX];
+const int MAX = 1504;
+const int dx[4] = {0, -1, 0, 1};
+const int dy[4] = {1, 0, -1, 0};
+int R, C, y, x, sy, sx, ret;
 char graph[MAX][MAX];
+bool swan[MAX][MAX], water[MAX][MAX];
 queue<pair<int, int>> waterQ, waterTmpQ, swanQ, swanTmpQ;
 string s;
 
-bool moveSwan() {
-    while (swanQ.size()) {
-        tie(y, x) = swanQ.front(); 
-        swanQ.pop();
-        for (int i=0; i<4; i++) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if (ny < 0 || nx < 0 || ny >= R || nx >= C || visitedSwan[ny][nx]) continue;
-            visitedSwan[ny][nx] = 1;
-            if (graph[ny][nx] == '.') swanQ.push({ny, nx});
-            else if (graph[ny][nx] == 'X') swanTmpQ.push({ny, nx});
-            else if (graph[ny][nx] == 'L') return true;
-        }
-    }
-    return false;
-}
-
-void waterMelting() {
+void meltWater() {
     while (waterQ.size()) {
         tie(y, x) = waterQ.front();
         waterQ.pop();
         for (int i=0; i<4; i++) {
             int ny = y + dy[i];
             int nx = x + dx[i];
-            if (ny < 0 || nx < 0 || ny >= R || nx >= C || visitedWater[ny][nx]) continue;
+            if (ny < 0 || nx < 0 || ny >= R || nx >= C || water[ny][nx]) continue;
             if (graph[ny][nx] == 'X') {
-                visitedWater[ny][nx] = 1;
-                waterTmpQ.push({ny, nx});
+                water[ny][nx] = 1;
                 graph[ny][nx] = '.';
+                waterTmpQ.push({ny, nx});
             }
         }
     }
 }
 
-void Qclear(queue<pair<int, int>> &q) {
+bool moveSwan() {
+    while (swanQ.size()) {
+        tie(y, x) = swanQ.front();
+        swanQ.pop();
+        for (int i=0; i<4; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if (ny < 0 || nx < 0 || ny >= R || nx >= C || swan[ny][nx]) continue;
+            swan[ny][nx] = 1;
+            if (graph[ny][nx] == 'L') return true;
+            if (graph[ny][nx] == '.') swanQ.push({ny, nx});
+            if (graph[ny][nx] == 'X') swanTmpQ.push({ny, nx});
+        }
+    }
+    return false;
+}
+
+void clearQ(queue<pair<int, int>> &q) {
     queue<pair<int, int>> empty;
     swap(q, empty);
 }
 
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 
-    cin >> R >> C;
+    cin>> R >> C;
     for (int i=0; i<R; i++) {
         cin >> s;
         for (int j=0; j<C; j++) {
             graph[i][j] = s[j];
-            if (s[j] == 'L') { swanY = i, swanX = j; }
-            if (s[j] == '.' || s[j] == 'L') { 
-                visitedWater[i][j] = 1;
+            if (graph[i][j] == 'L') { sy = i, sx = j; }
+            if (graph[i][j] == '.' || graph[i][j] == 'L') {
                 waterQ.push({i, j});
+                water[i][j] = 1;
             }
         }
     }
-    swanQ.push({swanY, swanX});
-    visitedSwan[swanY][swanX] = 1;
+
+    swanQ.push({sy, sx});
+    swan[sy][sx] = 1;
     while (true) {
         if (moveSwan()) break;
-        waterMelting();
-        swanQ = swanTmpQ; 
-        waterQ = waterTmpQ; 
-        Qclear(swanTmpQ);
-        Qclear(waterTmpQ);
+        meltWater();
+        waterQ = waterTmpQ;
+        swanQ = swanTmpQ;
+        clearQ(waterTmpQ);
+        clearQ(swanTmpQ);
         ret++;
     }
-    cout << ret << '\n';
 
+    cout << ret << '\n';
     return 0;
 }
