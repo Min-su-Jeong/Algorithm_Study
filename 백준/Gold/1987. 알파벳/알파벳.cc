5ed2299d6a73d370f24552f0883c8f7348075a9d
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-06-29 07:44:40 +0900

    [Gold IV] Title: 알파벳, Time: 372 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -1,25 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAX = 27;
+const int MAX = 21;
 const int dx[4] = {0, -1, 0, 1};
-const int dy[4] = {1, 0, -1, 0};
-int R, C, y, x, ret, graph[MAX][MAX];
-bool visited[MAX];
-string s;
+const int dy[4] = {-1, 0, 1, 0};
+int R, C, ret;
+char a[MAX][MAX];
 
-void go(int y, int x, int cnt) {
+void go(int y, int x, int num, int cnt) {
     ret = max(ret, cnt);
     for (int i=0; i<4; i++) {
         int ny = y + dy[i];
         int nx = x + dx[i];
         if (ny < 0 || nx < 0 || ny >= R || nx >= C) continue;
-        int next = graph[ny][nx];
-        if (!visited[next]) {
-            visited[next] = 1;
-            go(ny, nx, cnt+1);
-            visited[next] = 0;
-        }
+        int _next = 1 << (int)(a[ny][nx] - 'A');
+        if ((num & _next) == 0) go(ny, nx, num | _next, cnt+1);
     }
 }
 
@@ -28,15 +23,12 @@ int main() {
     cin.tie(NULL); cout.tie(NULL);
 
     cin >> R >> C;
-    for (int i=0; i<R; i++) {
-        cin >> s;
+    for(int i=0; i<R; i++) {
         for (int j=0; j<C; j++) {
-            graph[i][j] = s[j] - 'A';
+            cin >> a[i][j];
         }
     }
-
-    visited[graph[0][0]] = 1;
-    go(0, 0, 1);
-
+    go(0, 0, 1 << (int)(a[0][0] - 'A'), 1);
     cout << ret << '\n';
+    return 0;
 }
