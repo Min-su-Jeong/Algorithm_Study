b04b38daeeeb9a2a40991c21258a7aff372e35eb
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-07-01 09:58:09 +0900

    [Bronze IV] Title: 영수증, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int X, N, a, b, sum;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    cin >> X >> N;
+    for (int i=0; i<N; i++) {
+        cin >> a >> b;
+        sum += a*b;
+    }
+    if (sum == X) cout << "Yes\n";
+    else cout << "No\n";
+}
