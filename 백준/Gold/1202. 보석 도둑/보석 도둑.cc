7d421fef9ec12bdcee90227f029c604e52e1d820
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-07-04 14:53:45 +0900

    [Gold II] Title: 보석 도둑, Time: 144 ms, Memory: 15328 KB -BaekjoonHub

---
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ll N, K, ret;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> N >> K;
+    vector<pair<ll, ll>> v(N);
+    vector<ll> bag(K);
+
+    for (int i=0; i<N; i++) {
+        cin >> v[i].first >> v[i].second;
+    }
+    for (int i=0; i<K; i++) cin >> bag[i];
+    sort(v.begin(), v.end());
+    sort(bag.begin(), bag.end());
+    
+    priority_queue<ll> pq;
+    int j = 0;
+    for (int i=0; i<K; i++) {
+        while (j < N && v[j].first <= bag[i]) pq.push(v[j++].second);
+        if (pq.size()) {
+            ret += pq.top(); pq.pop();
+        } 
+    }
+    cout << ret << '\n';
+
+    return 0;
+}
