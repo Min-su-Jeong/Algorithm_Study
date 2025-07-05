1808e6810b98666e95a8b116a25ed57a9cc089dc
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-07-05 13:58:11 +0900

    [Gold III] Title: 소수의 연속합, Time: 20 ms, Memory: 13740 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
@@ -1,30 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAX = 4000001;
-int N, a[2000001], p, sum, l, r, ret; 
-bool notPrime[MAX];
+int N, a[2000001], p, l, r, sum, ret;
+bool prime[4000001];
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-
-    cin >> N;
+    memset(prime, true, sizeof(prime));
+    scanf("%d", &N);
     for (int i=2; i*i<=N; ++i) {
-        if (notPrime[i]) continue;
-        for (int j=i*i; j<=N; j+=i) notPrime[j] = 1;
+        if (!prime[i]) continue;
+        for (int j=i*i; j<=N; j+=i) {
+            prime[j] = 0;
+        }
     }
 
     for (int i=2; i<=N; i++) {
-        if (!notPrime[i]) a[p++] = i;
+        if (prime[i]) a[p++] = i;
     }
 
-    while (true) {
+    while(true) {
         if (sum >= N) sum -= a[l++];
         else if (r == p) break;
         else sum += a[r++];
         if (sum == N) ret++;
     }
+
     cout << ret << '\n';
     return 0;
 }
