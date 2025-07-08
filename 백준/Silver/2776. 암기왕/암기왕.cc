54ae2242f420b3dc0fba7ed452f118b2c4a97ac0
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-07-09 00:33:21 +0900

    [Silver IV] Title: 암기왕, Time: 524 ms, Memory: 9840 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.cc
+++ 백준/Silver/2776. 암기왕/암기왕.cc
@@ -3,6 +3,17 @@ using namespace std;
 
 int T, N, M;
 
+bool check(int target, vector<int> &v) {
+    int lo = 0, hi = v.size()-1, mid;
+    while (lo <= hi) {
+        int mid = (lo + hi) / 2;
+        if (target < v[mid]) hi = mid - 1;
+        else if (target == v[mid]) return 1;
+        else lo = mid + 1;
+    }
+    return 0;
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
@@ -18,8 +29,7 @@ int main() {
         vector<int> b(M);
         for (int i=0; i<M; i++) {
             cin >> b[i];
-            int pos = lower_bound(a.begin(), a.end(), b[i]) - a.begin();
-            if (a[pos] == b[i]) cout << "1\n";
+            if (check(b[i], a)) cout << "1\n";
             else cout << "0\n";
         }
     }
