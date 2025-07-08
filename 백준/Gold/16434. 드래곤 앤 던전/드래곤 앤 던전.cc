f012c56150f038ce4f98bc60c6d84b480ec5cd0d
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-07-08 22:38:17 +0900

    [Gold IV] Title: 드래곤 앤 던전, Time: 32 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.cc
+++ 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.cc
@@ -1,44 +1,29 @@
-#include <bits/stdc++.h>
-using namespace std; 
+#include<bits/stdc++.h>
+using namespace std;
 typedef long long ll;
 
-const int MAX = 130004;
-ll n, attack, t[MAX], a[MAX], h[MAX], l, r, ret;
+ll n, atk, t, a, h, H, mx, damage;
 
-bool check(ll mid) {
-	ll mxHP = mid;
-	ll init_attack = attack;
-	for(int i = 0; i < n; i++){
-		if(t[i] == 2) {
-			mid = min(mxHP, mid + h[i]);
-			init_attack += a[i];
-		} else {
-			ll cnt = h[i] / init_attack + (h[i] % init_attack ? 1 : 0); 
-			mid -= (cnt - 1) * a[i];
-		}   
-		if(mid <= 0) return false;
-	}
-	return true; 
-}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 
-int main(){ 
-	ios::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
+    cin>> n >> atk; 
+    for(int i = 0; i < n; i++) {
+        cin>> t >> a >> h;
+        if(t == 1) { 
+            damage = a * (ceil((double)h/atk) - 1); 
+            
+            if(H < damage) {
+                mx += damage - H;
+                H = 0; 
+            } else H -= damage;
+        } else {
+            atk += a;
+            H = min(H+h,mx);
+        }
+    }
+    cout << mx + 1 << "\n";
 
-	cin >> n >> attack;
-	for(int i = 0; i < n; i++){
-		cin >> t[i] >> a[i] >> h[i]; 
-	} 
-
-	l = 1, r = 1e18 + 4; 
-	while(l <= r){
-		ll mid = (l + r) / 2; 
-		if(check(mid)){
-			r = mid - 1; 
-			ret = mid;
-		} else l = mid + 1; 
-	}
-    cout << ret << '\n';
-
-	return 0;
-}
+    return 0;
+} 
