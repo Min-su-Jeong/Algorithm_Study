16859ac2085edfe6a994b9ea7ce91dd168a1eb7b
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-07-13 08:26:25 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -5,22 +5,21 @@ int t, n;
 string a, b;
 
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 
     cin >> t;
-    while(t--) {
+    while (t--) {
+        int ret = 1;
         map<string, int> mp;
+
         cin >> n;
         for (int i=0; i<n; i++) {
             cin >> a >> b;
             mp[b]++;
         }
-
-        long long ret = 1;
-        for (auto c: mp) {
-            ret *= ((long long) c.second + 1);
-        }
-        ret--;
-        cout << ret << '\n';
+        for (auto it: mp) ret *= (it.second+1);
+        cout << ret-1 << '\n';
     }
+    return 0;
 }
