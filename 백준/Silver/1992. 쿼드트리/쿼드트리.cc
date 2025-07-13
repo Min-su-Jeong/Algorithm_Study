f020b56887251f3aa2a28a13dd8cf29534e8dbcc
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-07-13 22:05:10 +0900

    [Silver I] Title: 쿼드트리, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.cc
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.cc
@@ -1,44 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAX = 65;
-char image[MAX][MAX];
-int N, size;
-string s;
+const int MAX = 68;
+int N; 
+char a[MAX][MAX];
 
-string quard(int y, int x, int size) {
-    if (size == 1) return string(1, image[y][x]);
+string quad(int y, int x, int size) {
+    if (size == 1) return string(1, a[y][x]);
 
-    char b = image[y][x];
+    char b = a[y][x];
     string ret = "";
 
     for (int i=y; i<y+size; i++) {
         for (int j=x; j<x+size; j++) {
-            if (b != image[i][j]) {
-                ret += '(';
-                ret += quard(y, x, size/2);
-                ret += quard(y, x+size/2, size/2);
-                ret += quard(y+size/2, x, size/2);
-                ret += quard(y+size/2, x+size/2, size/2);
-                ret += ')';
+            if (b != a[i][j]) {
+                ret += "(";
+                ret += quad(y, x, size/2);
+                ret += quad(y, x+size/2, size/2);
+                ret += quad(y+size/2, x, size/2);
+                ret += quad(y+size/2, x+size/2, size/2);
+                ret += ")";
                 return ret;
             }
         }
     }
-    return string(1, image[y][x]);
+    return string(1, a[y][x]);
 }
 
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-
-    cin >> N;
+    scanf("%d", &N);
     for (int i=0; i<N; i++) {
-        cin >> s;
-        for (int j=0; j<N; j++) {
-            image[i][j] = s[j];
-        }
+        scanf("%s", a[i]);
     }
+    cout << quad(0, 0, N) << '\n';
 
-    cout << quard(0, 0, N) << '\n';
     return 0;
 }
