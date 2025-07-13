27a9fe637a51a4aafbc928d29a8ebe0da2029643
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-07-13 22:39:35 +0900

    [Silver V] Title: 사과 담기 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.cc
+++ 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.cc
@@ -1,16 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int M, N, J, l, r, pos, ret;
+int N, M, J, pos, ret;
 
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
     cin >> N >> M >> J;
-    l = 1;
+
+    int l = 1, r = 1;
     for (int i=0; i<J; i++) {
         r = l + M - 1;
-        
+
         cin >> pos;
         if (l <= pos && pos <= r) continue;
         else {
@@ -18,12 +20,11 @@ int main() {
                 ret += (l - pos);
                 l = pos;
             } else {
-                l += (pos - r);
                 ret += (pos - r);
+                l += pos - r;
             }
         }
     }
     cout << ret << '\n';
-
     return 0;
 }
