b543ef3eb504b33d89d7179b78c2e6e0b1c25fb0
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-07-13 11:11:29 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -2,23 +2,21 @@
 using namespace std;
 typedef long long ll;
 
-int A, B, C;
+ll A, B, C;
 
-ll go(ll a, ll b) {
+ll modular(ll a, ll b) {
     if (b == 1) return a % C;
 
-    ll ret = go(a, b/2);
-    ret = (ret*ret) % C;
-    if (b%2) ret = (ret*a) % C;
+    ll ret = modular(a, b/2);
+    ret = (ret * ret) % C;
+
+    if (b % 2) ret = (ret * a) % C;
 
     return ret;
 }
 
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-
-    cin >> A >> B >> C;
-    cout << go(A, B) << "\n";
-
+    scanf("%lld %lld %lld", &A, &B, &C);
+    cout << modular(A, B) << '\n';
     return 0;
 }
