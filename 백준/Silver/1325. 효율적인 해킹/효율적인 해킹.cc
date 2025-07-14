ca5d99ec262c5b96767bb0f56eeda36f33a06257
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-07-15 07:18:15 +0900

    [Silver I] Title: 효율적인 해킹, Time: 2548 ms, Memory: 3512 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
@@ -2,35 +2,38 @@
 using namespace std;
 
 const int MAX = 10001;
-int N, M, A, B, mx, dp[MAX];
+int N, M, a, b, mx, dp[MAX];
 bool visited[MAX];
-vector<int> adj[MAX];
+vector<int> ret, adj[MAX];
 
-int dfs(int here) {
-    int ret = 1;
+int go(int here) {
+    int cnt = 1;
     visited[here] = 1;
+
     for (int there: adj[here]) {
         if (visited[there]) continue;
-        ret += dfs(there);
+        cnt += go(there);
     }
-    return ret;
+    return cnt;
 }
+
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 
     cin >> N >> M;
     for (int i=0; i<M; i++) {
-        cin >> A >> B;
-        adj[B].push_back(A);
+        cin >> a >> b;
+        adj[b].push_back(a);
     }
 
     for (int i=1; i<=N; i++) {
         memset(visited, 0, sizeof(visited));
-        dp[i] = dfs(i);
+        dp[i] = go(i);
         mx = max(mx, dp[i]);
     }
-    
-    for(int i=1; i<=N; i++) {
+
+    for (int i=1; i<=N; i++) {
         if (mx == dp[i]) cout << i << ' ';
     }
     return 0;
