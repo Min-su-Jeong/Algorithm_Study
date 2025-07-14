2acf7c3a494084a5b257d1126a12470eb4e4b51c
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-07-14 14:30:03 +0900

    [Silver IV] Title: 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.cc
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -4,27 +4,21 @@ using namespace std;
 int T;
 string s;
 
-bool check(string s) {
-    stack<char> stk;
-    for (char c: s) {
-        if (c == '(') stk.push(c);
-        else {
-            if (!stk.empty()) stk.pop();
-            else return false;
-        }
-    }
-    return stk.empty();
-}
-
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 
     cin >> T;
-    for (int i=0; i<T; i++) {
+    while (T--) {
         cin >> s;
-        if (check(s)) cout << "YES\n";
-        else cout << "NO\n";
+        
+        stack<char> stk;
+        for (char c: s) {
+            if (stk.size() && stk.top() == '(' && c == ')') stk.pop();
+            else stk.push(c);
+        }
+        if (stk.size()) cout << "NO\n";
+        else cout << "YES\n"; 
     }
-
     return 0;
 }
