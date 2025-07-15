44f0da007c4609c44e595e0c019353419125edaa
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-07-15 15:12:47 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 0 ms, Memory: 3044 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
@@ -2,34 +2,34 @@
 using namespace std;
 
 const int MAX = 64;
-const int _a[6][3] = {
-    {9, 3, 1},
-    {9, 1, 3},
-    {3, 9, 1},
-    {3, 1, 9},
+int N, a, b, c, visited[MAX][MAX][MAX];
+int damage[6][3] = {
+    {1, 3, 9},
     {1, 9, 3},
-    {1, 3, 9}
+    {3, 1, 9},
+    {3, 9, 1},
+    {9, 1, 3},
+    {9, 3, 1},
 };
 struct A {
     int a, b, c;
 };
-queue<A> q;
-int N, a[3], visited[MAX][MAX][MAX];
 
-int solve(int a, int b, int c) {
-    visited[a][b][c] = 1;
+int go(int a, int b, int c) {
+    queue<A> q;
     q.push({a, b, c});
-
-    while(q.size()) {
+    visited[a][b][c] = 1;
+    
+    while (q.size()) {
         int a = q.front().a;
         int b = q.front().b;
         int c = q.front().c;
         q.pop();
         if (visited[0][0][0]) break;
-        for (int i=0; i<6; i++) {
-            int na = max(0, a - _a[i][0]);
-            int nb = max(0, b - _a[i][1]);
-            int nc = max(0, c - _a[i][2]);
+        for(int i=0; i<6; i++) {
+            int na = max(0, a - damage[i][0]);
+            int nb = max(0, b - damage[i][1]);
+            int nc = max(0, c - damage[i][2]);
             if (visited[na][nb][nc]) continue;
             visited[na][nb][nc] = visited[a][b][c] + 1;
             q.push({na, nb, nc});
@@ -39,9 +39,10 @@ int solve(int a, int b, int c) {
 }
 
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    scanf("%d", &N);
+    scanf("%d %d %d", &a, &b, &c);
 
-    cin >> N;
-    for (int i=0; i<N; i++) cin >> a[i];
-    cout << solve(a[0], a[1], a[2]) << '\n';
+    cout << go(a, b, c) << '\n';
+    
+    return 0;
 }
