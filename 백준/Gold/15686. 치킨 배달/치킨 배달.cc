b792b7c8a7b4d59e29cd6623c07920165c9d7865
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-07-15 09:54:04 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2172 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -2,51 +2,50 @@
 using namespace std;
 
 const int MAX = 51;
-int N, M, graph[MAX][MAX], result = INT_MAX;
-vector<vector<int>> chickenList;
-vector<pair<int, int>> home, chicken;
+int N, M, a[MAX][MAX], ret=987654321;
+vector<vector<int>> selectedChicken;
+vector<pair<int, int>> chicken, home;
 
-void combi(int start, vector<int> v) {
+void comb(int start, vector<int> v) {
     if (v.size() == M) {
-        chickenList.push_back(v);
+        selectedChicken.push_back(v);
         return;
     }
-
     for (int i=start+1; i<chicken.size(); i++) {
         v.push_back(i);
-        combi(i, v);
+        comb(i, v);
         v.pop_back();
     }
-    return;
 }
 
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 
     cin >> N >> M;
     for (int i=0; i<N; i++) {
         for (int j=0; j<N; j++) {
-            cin >> graph[i][j];
-            if (graph[i][j] == 1) home.push_back({i, j});
-            if (graph[i][j] == 2) chicken.push_back({i, j});
+            cin >> a[i][j];
+            if (a[i][j] == 1) home.push_back({i, j});
+            if (a[i][j] == 2) chicken.push_back({i, j});
         }
     }
 
     vector<int> v;
-    combi(-1, v);
-    for (vector<int> cList: chickenList) {
-        int ret = 0;
-        for (pair<int, int> h: home) {
-            int _min = INT_MAX;
-            for (int ch: cList) {
-                int _dist = abs(h.first - chicken[ch].first) + abs(h.second - chicken[ch].second);
-                _min = min(_min, _dist);
+    comb(-1, v);
+    for (vector<int> selected: selectedChicken) {
+        int sum = 0;
+        for (auto h: home) {
+            int mn = 987654321;
+            for (int i: selected) {
+                int dist = abs(h.first - chicken[i].first) + abs(h.second - chicken[i].second);
+                mn = min(mn, dist);
             }
-            ret += _min;
+            sum += mn;
         }
-        result = min(result, ret);
+        ret = min(ret, sum);
     }
-    cout << result << '\n';
+    cout << ret << '\n';
 
     return 0;
 }
