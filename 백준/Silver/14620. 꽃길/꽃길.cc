662c3cdee3a5ca87358f9303b1c9f14bc3ceeb28
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-07-16 22:49:20 +0900

    [Silver II] Title: 꽃길, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14620. 꽃길/꽃길.cc
+++ 백준/Silver/14620. 꽃길/꽃길.cc
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAX = 20;
-const int dx[4] = {1, 0, -1, 0};
-const int dy[4] = {0, 1, 0, -1};
+const int MAX = 14;
+const int dx[4] = {0, -1, 0, 1};
+const int dy[4] = {1, 0, -1, 0};
 int N, graph[MAX][MAX], ret=INT_MAX;
 bool visited[MAX][MAX];
 
@@ -24,8 +24,8 @@ int setFlower(int y, int x) {
     for (int i=0; i<4; i++) {
         int ny = y + dy[i];
         int nx = x + dx[i];
-        visited[ny][nx] = 1;
         s += graph[ny][nx];
+        visited[ny][nx] = 1;
     }
     return s;
 }
@@ -39,15 +39,15 @@ void eraseFlower(int y, int x) {
     }
 }
 
-void flower(int cnt, int hap) {
-    if (cnt == 3) {
+void flower(int depth, int hap) {
+    if (depth == 3) {
         ret = min(ret, hap);
         return;
     }
     for (int i=0; i<N; i++) {
         for (int j=0; j<N; j++) {
             if (check(i, j)) {
-                flower(cnt+1, hap+setFlower(i, j));
+                flower(depth+1, hap+setFlower(i, j));
                 eraseFlower(i, j);
             }
         }
@@ -56,14 +56,15 @@ void flower(int cnt, int hap) {
 
 int main() {
     ios_base::sync_with_stdio(false);
-    
+    cin.tie(NULL); cout.tie(NULL);
+
     cin >> N;
     for (int i=0; i<N; i++) {
         for (int j=0; j<N; j++) {
             cin >> graph[i][j];
         }
     }
-    
+
     flower(0, 0);
 
     cout << ret << '\n';
