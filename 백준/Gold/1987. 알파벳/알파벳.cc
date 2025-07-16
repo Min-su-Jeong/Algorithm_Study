e9bcf111238d96913a521b74722e0b5b06a870ff
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-07-16 17:12:08 +0900

    [Gold IV] Title: 알파벳, Time: 360 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -4,18 +4,19 @@ using namespace std;
 const int MAX = 21;
 const int dx[4] = {0, -1, 0, 1};
 const int dy[4] = {-1, 0, 1, 0};
-int R, C, ret;
-char a[MAX][MAX];
+int R, C, ret, a[MAX][MAX];
+string s;
 
-void go(int y, int x, int num, int cnt) {
+int go(int y, int x, int num, int cnt) {
     ret = max(ret, cnt);
     for (int i=0; i<4; i++) {
         int ny = y + dy[i];
         int nx = x + dx[i];
-        if (ny < 0 || nx < 0 || ny >= R || nx >= C) continue;
-        int _next = 1 << (int)(a[ny][nx] - 'A');
+        if (ny < 0 || ny >= R || nx < 0 || nx >= C) continue;
+        int _next = (1 << a[ny][nx]);
         if ((num & _next) == 0) go(ny, nx, num | _next, cnt+1);
     }
+    return ret;
 }
 
 int main() {
@@ -23,12 +24,14 @@ int main() {
     cin.tie(NULL); cout.tie(NULL);
 
     cin >> R >> C;
-    for(int i=0; i<R; i++) {
+    for (int i=0; i<R; i++) {
+        cin >> s;
         for (int j=0; j<C; j++) {
-            cin >> a[i][j];
+            a[i][j] = s[j] - 'A';
         }
     }
-    go(0, 0, 1 << (int)(a[0][0] - 'A'), 1);
-    cout << ret << '\n';
+
+    cout << go(0, 0, (1 << a[0][0]), 1) << '\n';
+
     return 0;
 }
