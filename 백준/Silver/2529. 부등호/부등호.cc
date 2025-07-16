1e7ceda153f5e2429a9cc4e43d3e58fa76368f93
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-07-16 17:44:46 +0900

    [Silver I] Title: 부등호, Time: 4 ms, Memory: 3692 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.cc
+++ 백준/Silver/2529. 부등호/부등호.cc
@@ -1,40 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int k, visited[10];
+int k;
 char a[20];
+bool visited[10];
 vector<string> ret;
 
-bool check(char x, char y, char op){ 
-	if(x < y && op == '<') return true; 
-	if(x > y && op == '>') return true; 
-	return false; 
+bool check(char x, char y, char op) {
+	if (x < y && op == '<') return true;
+	if (x > y && op == '>') return true;
+	return false;
 }
-void go(int idx, string num){ 
-	if(idx == k+1){ 
-		ret.push_back(num); 
-        return;
+
+void go(int depth, string num) {
+	if (depth == k+1) {
+		ret.push_back(num);
+		return;
 	}
 
-	for(int i = 0; i <= 9; i++){
-		if(visited[i]) continue; 
-		if(idx == 0 || check(num[idx - 1], i + '0', a[idx - 1])){
+	for (int i=0; i<10; i++) {
+		if (visited[i]) continue;
+		if (depth == 0 || check(num[depth-1], i+'0', a[depth-1])) {
 			visited[i] = 1;
-			go(idx + 1, num + to_string(i));
+			go(depth+1, num + to_string(i));
 			visited[i] = 0;
 		}
 	}
 	return;
 }
 
-int main(){
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); 
-    cin >> k; 
-    for(int i = 0; i < k; i++) cin >> a[i]; 
-    
-    go(0, "");
-    sort(ret.begin(), ret.end());
-    
-    cout << ret[ret.size() - 1] << "\n" << ret[0] << "\n";
-    return 0;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> k;
+	for (int i=0; i<k; i++) cin >> a[i];
+
+	go(0, "");
+	sort(ret.begin(), ret.end());
+
+	cout << ret[ret.size()-1] << '\n' << ret[0] << '\n';
+	return 0;
 }
