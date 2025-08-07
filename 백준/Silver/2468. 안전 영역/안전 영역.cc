00211892ec00b7e2584c0c59f59ac3ce6e2d1122
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-08-08 08:38:34 +0900

    [Silver I] Title: 안전 영역, Time: 20 ms, Memory: 2072 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.cc
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -1,45 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAX = 101;
-int dx[4] = {-1, 1, 0, 0};
-int dy[4] = {0, 0, -1, 1};
-int N, ret=1; // 반례 방지
-int graph[MAX][MAX];
+const int MAX = 104;
+const int dx[4] = {0, -1, 0, 1};
+const int dy[4] = {-1, 0, 1, 0};
+int N, a[MAX][MAX], ret=1;
 bool visited[MAX][MAX];
 
-void dfs(int y, int x, int d) {
-    visited[y][x] = true;
+void bfs(int y, int x, int h) {
+    queue<pair<int, int>> q;
+    q.push({y, x});
 
-    for (int i=0; i<4; i++) {
-        int ny = y + dy[i];
-        int nx = x + dx[i];
-
-        if (ny < 0 || nx < 0 || nx >= N || ny >= N) continue;
-        if (graph[ny][nx] > d && !visited[ny][nx]) {
-            dfs(ny, nx, d);
+    while (q.size()) {
+        tie(y, x) = q.front();
+        q.pop();
+        for (int i=0; i<4; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if (ny < 0 || ny >= N || nx < 0 || nx >= N) continue;
+            if (visited[ny][nx] || a[ny][nx] <= h) continue;
+            visited[ny][nx] = 1;
+            q.push({ny, nx});
         }
     }
 }
 
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 
     cin >> N;
     for (int i=0; i<N; i++) {
         for (int j=0; j<N; j++) {
-            cin >> graph[i][j];
+            cin >> a[i][j];
         }
     }
 
-    for (int d=1; d<101; d++) {
-        fill(&visited[0][0], &visited[0][0] + MAX * MAX, 0);
+    for (int h=1; h<=100; h++) {
         int cnt = 0;
-
+        memset(visited, 0, sizeof(visited));
         for (int i=0; i<N; i++) {
             for (int j=0; j<N; j++) {
-                if (graph[i][j] > d && !visited[i][j]) {
-                    dfs(i, j, d);
+                if (a[i][j] > h) {
+                    if (visited[i][j]) continue;
+                    visited[i][j] = 1;
+                    bfs(i, j, h);
                     cnt++;
                 }
             }
@@ -47,5 +52,6 @@ int main() {
         ret = max(ret, cnt);
     }
     cout << ret << '\n';
+    
     return 0;
 }
