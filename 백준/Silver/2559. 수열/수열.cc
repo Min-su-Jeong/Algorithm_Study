44f7e2cb630e19e9014f93be3ca803f89c913719
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-08-07 21:27:39 +0900

    [Silver III] Title: 수열, Time: 8 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -1,19 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, K, num, psum[100001], ret = -10000004;
-
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 
+    int N, K, num, ret=-INT_MAX;
     cin >> N >> K;
+
+    int psum[N];
+    memset(psum, 0, sizeof(psum));
     for (int i=1; i<=N; i++) {
         cin >> num;
         psum[i] = psum[i-1] + num;
     }
+
     for (int i=K; i<=N; i++) {
         ret = max(ret, psum[i] - psum[i-K]);
     }
     cout << ret << '\n';
+    
     return 0;
 }
