af16b95927d49b0fdba953c8ea545b54fdef404b
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-08-08 17:12:47 +0900

    [Silver IV] Title: 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.cc
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -10,15 +10,15 @@ int main() {
 
     cin >> T;
     while (T--) {
-        cin >> s;
-        
         stack<char> stk;
+
+        cin >> s;
         for (char c: s) {
             if (stk.size() && stk.top() == '(' && c == ')') stk.pop();
             else stk.push(c);
         }
         if (stk.size()) cout << "NO\n";
-        else cout << "YES\n"; 
+        else cout << "YES\n";
     }
     return 0;
 }
