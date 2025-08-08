dda8c6862bd7f5435b5e0ccd2642719c10fdee31
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-08-08 22:38:21 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2168 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -1,43 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAX = 51;
-int N, M, a[MAX][MAX], ret=987654321;
+const int INF = 987654321;
+const int MAX = 54;
+int N, M, a[MAX][MAX], ret = INF;
+vector<pair<int, int>> home, chicken;
 vector<vector<int>> selectedChicken;
-vector<pair<int, int>> chicken, home;
 
-void comb(int start, vector<int> v) {
+void combi(int idx, vector<int> &v) {
     if (v.size() == M) {
         selectedChicken.push_back(v);
         return;
     }
-    for (int i=start+1; i<chicken.size(); i++) {
+    for (int i=idx+1; i<chicken.size(); i++) {
         v.push_back(i);
-        comb(i, v);
+        combi(i, v);
         v.pop_back();
     }
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-
     cin >> N >> M;
     for (int i=0; i<N; i++) {
         for (int j=0; j<N; j++) {
             cin >> a[i][j];
             if (a[i][j] == 1) home.push_back({i, j});
-            if (a[i][j] == 2) chicken.push_back({i, j});
+            else if (a[i][j] == 2) chicken.push_back({i, j});
         }
     }
 
     vector<int> v;
-    comb(-1, v);
-    for (vector<int> selected: selectedChicken) {
+    combi(-1, v);
+
+    for (vector<int> ch: selectedChicken) {
         int sum = 0;
         for (auto h: home) {
-            int mn = 987654321;
-            for (int i: selected) {
+            int mn = INF;
+            for (int i: ch) {
                 int dist = abs(h.first - chicken[i].first) + abs(h.second - chicken[i].second);
                 mn = min(mn, dist);
             }
@@ -46,6 +45,5 @@ int main() {
         ret = min(ret, sum);
     }
     cout << ret << '\n';
-
     return 0;
 }
