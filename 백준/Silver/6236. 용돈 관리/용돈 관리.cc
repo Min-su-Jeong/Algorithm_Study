88e500c2e4121724ad79c4f5ac3ed38577d3d7c2
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-08-09 22:15:49 +0900

    [Silver I] Title: 용돈 관리, Time: 16 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/6236. 용돈 관리/용돈 관리.cc
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.cc
@@ -1,13 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
 
-const int MAX = 100001;
-ll N, M, mx, a[MAX], ret=INT_MAX;
+const int MAX = 100004;
+int N, M, mx, ret, a[MAX];
 
 bool check(int mid) {
-    int cnt = 1;
-    ll tmp = mid;
+    int cnt = 1, tmp = mid;
     for (int i=0; i<N; i++) {
         if (mid - a[i] < 0) {
             mid = tmp;
@@ -15,7 +13,7 @@ bool check(int mid) {
         }
         mid -= a[i];
     }
-    return cnt <= M;
+    return M >= cnt;
 }
 
 int main() {
@@ -25,15 +23,14 @@ int main() {
         mx = max(mx, a[i]);
     }
 
-    ll l=mx, r=1000000001;
+    int l = mx, r = 100000 * 10000 + 4;
     while (l <= r) {
-        ll mid = (l+r) / 2;
+        int mid = (l + r) / 2;
         if (check(mid)) {
             ret = mid;
             r = mid - 1;
         } else l = mid + 1;
     }
     cout << ret << '\n';
-
     return 0;
 }
