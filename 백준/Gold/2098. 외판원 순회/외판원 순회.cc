599921f49b19be53263113cd5a92ee261c08a46a
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2025-08-11 20:08:37 +0900

    [Gold I] Title: 외판원 순회, Time: 28 ms, Memory: 6116 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.cc
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.cc
@@ -1,22 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAX = 16;
 const int INF = 987654321;
-int N, w[MAX][MAX], dp[MAX][1 << MAX];
+const int MAX = 16;
+int N, W[MAX][MAX], dp[MAX][1 << MAX];
 
 int tsp(int here, int visited) {
     if (visited == (1 << N) - 1) {
-        return w[here][0] ? w[here][0] : INF;
+        return W[here][0] ? W[here][0] : INF;
     }
     int &ret = dp[here][visited];
     if (ret != -1) return ret;
-
     ret = INF;
     for (int i=0; i<N; i++) {
         if (visited & (1 << i)) continue;
-        if (w[here][i] == 0) continue;
-        ret = min(ret, tsp(i, visited | (1 << i)) + w[here][i]);
+        if (W[here][i] == 0) continue;
+        ret = min(ret, tsp(i, visited | (1 << i)) + W[here][i]);
     }
     return ret;
 }
@@ -25,15 +24,14 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 
-    cin >> N;
+    cin >> N; 
     for (int i=0; i<N; i++) {
         for (int j=0; j<N; j++) {
-            cin >> w[i][j];
+            cin >> W[i][j];
         }
     }
-    
     memset(dp, -1, sizeof(dp));
     cout << tsp(0, 1) << '\n';
-
+    
     return 0;
 }
