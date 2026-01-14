5220e5f85df5aa6de61e0756c5a59278ed6c3400
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2026-01-14 23:43:51 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.42 ms, Memory: 9.19 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -0,0 +1,19 @@
+import re
+
+def solution(new_id):
+    new_id = new_id.lower()
+    new_id = re.sub('[^a-z0-9\-_.]','', new_id)
+    new_id = re.sub(r'\.{2,}','.',new_id)
+    new_id = new_id.strip('.')
+    if len(new_id) == 0:
+        new_id = "a"
+    if len(new_id) >= 16:
+        new_id = new_id[:15]
+        new_id = new_id.strip('.')
+    if len(new_id) <= 2:
+        c = new_id[-1]
+        while len(new_id) < 3:
+            new_id += c
+            
+    return new_id
+            
