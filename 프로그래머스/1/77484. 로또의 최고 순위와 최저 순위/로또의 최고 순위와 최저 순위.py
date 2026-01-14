1867558e095fdd6a2b9e991fc3f7f9e687b5b51f
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2026-01-14 23:12:37 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -2,10 +2,7 @@ def solution(lottos, win_nums):
     ret = []
     rankDict = {6: 1, 5: 2, 4: 3, 3: 4, 2: 5, 1: 6, 0: 6}
     
-    zeroCnt = 0
-    for l in lottos:
-        if l == 0:
-            zeroCnt += 1
+    zeroCnt = lottos.count(0)
             
     minMatch = len(set(lottos) & set(win_nums))
     maxMatch = minMatch + zeroCnt
