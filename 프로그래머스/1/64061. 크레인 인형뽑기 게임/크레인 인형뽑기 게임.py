c8daf57dbea9c1a0932f7c3b0d3a874768dc8c52
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2026-01-17 12:52:57 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.39 ms, Memory: 9.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -0,0 +1,24 @@
+def solution(board, moves):
+    ret = 0
+    stk = []
+    size = len(board)
+    
+    for move in moves:
+        move -= 1 # 리스트 인덱스 맞추기 위함
+        for i in range(size):
+            doll = board[i][move]
+            
+            if doll != 0:
+                stk.append(doll)
+                board[i][move] = 0
+                break
+                
+        if len(stk) >= 2 and stk[-1] == stk[-2]:
+            ret += 2
+            stk.pop()
+            stk.pop()
+        
+    return ret
+        
+        
+            
