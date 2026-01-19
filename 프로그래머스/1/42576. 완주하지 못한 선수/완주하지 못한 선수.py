0e44132ffa276fa235ca53f230c57e1edd3bb9e9
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2026-01-19 20:00:14 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 57.31 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,12 +1,14 @@
 def solution(participant, completion):
-    # 두 목록을 정렬
-    participant.sort()
-    completion.sort()
-
-    # 정렬된 두 목록을 비교
-    for p, c in zip(participant, completion):
-        if p != c:
-            return p  # 참가자와 완주자가 다르면 참가자가 완주하지 못한 선수
-
-    # 모든 완주자가 같다면, 마지막 참가자가 완주하지 못한 선수
-    return participant[-1]
+    player = {}
+    tmp = 0
+    for p in participant:
+        player[hash(p)] = p
+        tmp += int(hash(p))
+        
+    for c in completion:
+        tmp -= int(hash(c))
+        
+    ret = player[tmp]
+    
+    return ret
+        
