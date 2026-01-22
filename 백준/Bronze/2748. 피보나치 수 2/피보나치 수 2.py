b2af048e24b4cd09b0841650d5ff11a6297917d4
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2026-01-22 10:23:25 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,10 +1,28 @@
+"""
+n의 최댓값: 90
+
+풀이 전략 1
+- 피보나치 순환 함수 작성
+- n 값을 입력받아 n번쨰 피보나치 수 출력
+- 시간 복잡도: O(2^N)
+
+풀이 전략 2 (채택)
+- DP 이용
+- 시간 복잡도: O(N)
+"""
+
+# Input
 n = int(input())
 
-dp = [0] * 91
+# Variables
+MAX = 91
+dp = [0] * MAX 
 dp[1] = 1
 dp[2] = 1
 
-for i in range(3, 90+1):
+# Logic
+for i in range(3, n+1):
     dp[i] = dp[i-1] + dp[i-2]
-    
+
+# Output
 print(dp[n])
