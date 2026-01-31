264976bc16e2a9a4ff0e125ff19b696b58b4ecbd
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2026-01-31 15:59:46 +0900

    [Silver I] Title: 미로 탐색, Time: 60 ms, Memory: 34936 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,24 +1,29 @@
+import sys; input = lambda: sys.stdin.readline().rstrip()
 from collections import deque
 
-def bfs(x, y):
+N, M = map(int, input().split())
+graph = [list(map(int, input())) for _ in range(N)]
+
+dy = [-1, 0, 1, 0]
+dx = [0, -1, 0, 1]
+
+def bfs(y, x):
     q = deque()
-    q.append((x, y))
+    q.append((y, x))
+
     while q:
-        x, y = q.popleft()
+        y, x = q.popleft()
         for i in range(4):
-            nx = x + dx[i]
             ny = y + dy[i]
-            
-            if 0 <= nx < n and 0 <= ny < m and miro[nx][ny] == 1:
-                q.append((nx, ny))
-                miro[nx][ny] = miro[x][y] + 1
-                    
-    return miro[n-1][m-1]
+            nx = x + dx[i]
+            if ny < 0 or ny >= N or nx < 0 or nx >= M:
+                continue
+            if graph[ny][nx] != 1:
+                continue
 
-n, m = map(int, input().split())
-miro = [list(map(int, input().rstrip())) for _ in range(n)]
+            graph[ny][nx] = graph[y][x] + 1
+            q.append((ny, nx))
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1] # 상, 하, 좌, 우
+bfs(0, 0)
 
-print(bfs(0, 0))
+print(graph[N-1][M-1])
