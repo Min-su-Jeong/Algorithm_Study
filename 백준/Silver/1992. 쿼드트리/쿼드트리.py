167c3a02686e850757032cebcedec65aafee0f59
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2026-02-08 00:28:19 +0900

    [Silver I] Title: 쿼드트리, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.py
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.py
@@ -1,20 +1,29 @@
 import sys
+input = lambda: sys.stdin.readline().rstrip()
+sys.setrecursionlimit(10**6)
 
-def quadTree(x, y, n):
-    check = img[x][y]   
-    for i in range(x, x+n):
-        for j in range(y, y+n):
+def quad(y, x, size):
+    if size == 1:
+        return str(img[y][x])
+    
+    check = img[y][x]
+    ret = ""
+
+    for i in range(y, y+size):
+        for j in range(x, x+size):
             if check != img[i][j]:
-                print('(', end='')
-                quadTree(x, y, n//2)
-                quadTree(x, y+n//2, n//2)
-                quadTree(x+n//2, y, n//2)
-                quadTree(x+n//2, y+n//2, n//2)
-                print(')', end='')
-                return
-    print(check, end='')
+                ret += '('
+                ret += quad(y, x, size//2)
+                ret += quad(y, x+size//2, size//2)
+                ret += quad(y+size//2, x, size//2)
+                ret += quad(y+size//2, x+size//2 , size//2)
+                ret += ')'
+                return ret
+    
+    return str(img[y][x])
 
-input = sys.stdin.readline
-n = int(input())
-img = [input().rstrip() for _ in range(n)]
-quadTree(0, 0, n)
+if __name__ == "__main__":
+    N = int(input())
+    img = [list(map(int, input())) for _ in range(N)]
+    
+    print(quad(0, 0, N))
