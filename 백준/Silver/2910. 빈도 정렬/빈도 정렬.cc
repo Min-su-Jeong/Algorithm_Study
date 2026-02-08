01757956064f19541f86016cd40954e95b8628a2
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2026-02-08 12:00:41 +0900

    [Silver III] Title: 빈도 정렬, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
@@ -1,30 +1,13 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int N, C, a[1004];
-map<int, int> mp, mp_first;
-vector<pair<int, int>> v;
-
-bool cmp(pair<int, int> a, pair<int, int> b) {
-    if (a.second == b.second) return mp_first[a.first] < mp_first[b.first];
-    return a.second > b.second;
-}
-
-int main() {
-    scanf("%d %d", &N, &C);
-    for (int i=0; i<N; i++) {
-        scanf("%d", a+i);
-        mp[a[i]]++;
-        if (mp_first[a[i]] == 0) mp_first[a[i]] = i+1;
-    }
-
-    for (auto it: mp) v.push_back({it.first, it.second});
-    sort(v.begin(), v.end(), cmp);
-
-    for (auto it: v) {
-        for (int i=0; i<it.second; i++) {
-            cout << it.first << ' ';
-        }
-    }
-    return 0;
-}
+<html>
+<head><title>504 Gateway Time-out</title></head>
+<body>
+<center><h1>504 Gateway Time-out</h1></center>
+<hr><center>nginx/1.22.1</center>
+</body>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
