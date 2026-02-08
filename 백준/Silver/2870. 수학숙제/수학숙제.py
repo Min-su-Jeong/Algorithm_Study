dff026626f73d0ff12050002e6382f497a58011b
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2026-02-08 22:00:23 +0900

    [Silver IV] Title: 수학숙제, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2870. 수학숙제/수학숙제.py
+++ 백준/Silver/2870. 수학숙제/수학숙제.py
@@ -3,16 +3,14 @@ ret = []
 
 for _ in range(N):
     s = input()
-    
+
     digit = ""
     for i in range(len(s)):
         if s[i].isdigit():
             digit += s[i]
-        else:
-            if digit:
-                ret.append(int(digit))
-                digit = ""
-
+        elif digit:
+            ret.append(int(digit))
+            digit = ""
     if digit:
         ret.append(int(digit))
 
