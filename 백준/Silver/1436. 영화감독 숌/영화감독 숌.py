6c3b462ef99b79c2c02425a2ccb2618c440344c1
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2026-02-10 23:04:52 +0900

    [Silver V] Title: 영화감독 숌, Time: 456 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,11 +1,9 @@
-# 브루트 포스(Brute Force) 접근
-n = int(input())
-num = 666
-cnt = 0
+N = int(input())
 
-while n: # n이 0(False)이 아닐 때 까지 반복
-    if "666" in str(num): # 숫자에 "666"이 포함되면
-        n -= 1
-    num += 1
+ret = 666
+while N:
+    if "666" in str(ret):
+        N -= 1
+    ret += 1
     
-print(num-1) # while 문을 빠져나올 때 다음 숫자가 출력되므로 -1 필요  
+print(ret-1)
