7fe213883d985940e2fcaf45e5740f3ecec7fe68
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2026-02-10 23:16:41 +0900

    [Silver IV] Title: 괄호, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,18 +1,18 @@
-n = int(input())
-
-for _ in range(n):
-    stack = []
-    for s in input():
-        if s == '(':
-            stack.append('(')
-        else: # ')' 인 경우
-            if stack: # stack에 괄호가 있는 경우
-                stack.pop()
-            else: # stack에 괄호가 없는 경우
-                print('NO')
-                break
-    else: # for문에서 break가 발생하지 않은 경우
-        if not stack:
-            print('YES')
+def check(s: str):
+    stk = []
+    for c in s:
+        if c == '(': stk.append(c)
         else:
-            print('NO')
+            if stk: stk.pop()
+            else: return False
+    
+    return len(stk) == 0
+
+if __name__ == "__main__":
+    N = int(input())
+
+    for _ in range(N):
+        s = input()
+
+        if check(s): print("YES")
+        else: print("NO")
