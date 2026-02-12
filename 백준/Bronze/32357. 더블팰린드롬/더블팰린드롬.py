910e42a0c782fcb9d9689ecfab6965822cc62380
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2026-02-12 11:00:02 +0900

    [Bronze I] Title: 더블팰린드롬, Time: 100 ms, Memory: 42532 KB -BaekjoonHub

--- 백준/Bronze/32357. 더블팰린드롬/더블팰린드롬.py
+++ 백준/Bronze/32357. 더블팰린드롬/더블팰린드롬.py
@@ -1,17 +1,13 @@
 import sys
-input = sys.stdin.readline
+input = lambda: sys.stdin.readline().rstrip()
 
 def check(s):
     return s == s[::-1]
 
 N = int(input().strip())
+
 ret = 0
-
-strings = []
-for _ in range(N):
-    s = input().strip()
-    strings.append(s)
-
+strings = [input() for _ in range(N)]
 for s in strings:
     if check(s):
         ret += 1
