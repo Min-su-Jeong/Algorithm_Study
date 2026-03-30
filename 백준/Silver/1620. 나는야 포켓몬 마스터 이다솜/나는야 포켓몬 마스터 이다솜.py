9102767bbc64fe7cbd777accbbfc44eacd6e91f9
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2026-03-30 15:47:26 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 252 ms, Memory: 51528 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -1,16 +1,19 @@
 import sys
-input = sys.stdin.readline
-n, m = map(int, input().split())
-d = {}
+input = lambda: sys.stdin.readline().strip()
 
-for i in range(1, n+1):
-    a = input().rstrip()
-    d[i] = a
-    d[a] = i
+N, M = map(int, input().split())
 
-for i in range(m):
-    quest = input().rstrip()
-    if quest.isdigit():
-        print(d[int(quest)])
+mpI = {}
+mpS = {}
+for i in range(1, N+1):
+    s = input()
+    mpI[i] = s
+    mpS[s] = i
+
+for _ in range(M):
+    s = input()
+
+    if s.isdigit():
+        print(mpI[int(s)])
     else:
-        print(d[quest])
+        print(mpS[s])
