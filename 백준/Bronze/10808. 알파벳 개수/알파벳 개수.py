75e02f0c4caa1a51e73f1d1a8d952df9443fbb16
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2026-03-30 14:36:52 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
@@ -1,9 +1,8 @@
+string = input()
 alpha = [0] * 26
 
-S = input()
-
-for s in S:
-    alpha[ord(s) - ord('a')] += 1
+for s in string:
+    alpha[ord(s) - 97] += 1
 
 for a in alpha:
     print(a, end=' ')
