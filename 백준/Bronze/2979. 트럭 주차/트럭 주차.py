a04d1bf6d534d4b4f5ebbe2704bdb37525b62db1
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2026-03-30 14:50:39 +0900

    [Bronze II] Title: 트럭 주차, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.py
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.py
@@ -1,24 +1,16 @@
 A, B, C = map(int, input().split())
 
-parking = [0] * 101
-minNum = 101
-maxNum = 0
+mp = {}
 ret = 0
 
 for _ in range(3):
     s, e = map(int, input().split())
-    minNum = min(minNum, s)
-    maxNum = max(maxNum, e)
-
     for i in range(s, e):
-        parking[i] += 1
-        
-for i in range(minNum, maxNum):
-    if parking[i] == 1:
-        ret += A
-    elif parking[i] == 2:
-        ret += 2 * B
-    elif parking[i] == 3:
-        ret += 3 * C
+        mp[i] = mp.get(i, 0) + 1
+
+for v in mp.values():
+    if v == 1: ret += A
+    elif v == 2: ret += 2 * B
+    elif v == 3: ret += 3 * C
 
 print(ret)
