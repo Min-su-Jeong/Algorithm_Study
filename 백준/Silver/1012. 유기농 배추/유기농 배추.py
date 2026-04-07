551ff4e1501307ef4d7f66596b40323dea13e49a
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2026-04-07 15:22:10 +0900

    [Silver II] Title: 유기농 배추, Time: 44 ms, Memory: 32776 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,41 +1,40 @@
-
-import sys; input = lambda: sys.stdin.readline().rstrip()
+import sys
+input = lambda: sys.stdin.readline().rstrip()
 sys.setrecursionlimit(10**7)
+
+dy = [-1, 0, 1, 0]
+dx = [0, 1, 0, -1]
 
 T = int(input())
 
-dy = [-1, 0, 1, 0]
-dx = [0, -1, 0, 1]
-
 def dfs(y, x):
-    visited[y][x] = True
+    graph[y][x] = 0
+
     for i in range(4):
         ny = y + dy[i]
         nx = x + dx[i]
-        if ny < 0 or nx < 0 or ny >= N or nx >= M: continue
-        if graph[ny][nx] == 0 or visited[ny][nx]: continue
+        if ny < 0 or ny >= N or nx < 0 or nx >= M: 
+            continue
+        if graph[ny][nx] == 1:
+            dfs(ny, nx)
 
-        dfs(ny, nx)
-            
+    return 1
+
 while T:
     M, N, K = map(int, input().split())
-
+    graph = [[0 for _ in range(M)] for _ in range(N)]
     ret = 0
-    visited = [[0] * M for _ in range(N)]
-    graph = [[0] * M for _ in range(N)]
 
-    for i in range(K):
+    for _ in range(K):
         x, y = map(int, input().split())
         graph[y][x] = 1
 
     for i in range(N):
         for j in range(M):
-            if graph[i][j] == 0 or visited[i][j]:
+            if graph[i][j] == 0:
                 continue
-            
-            dfs(i, j)
-            ret += 1
+            ret += dfs(i, j)
 
     print(ret)
-    
+
     T -= 1
