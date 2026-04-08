7b46518d083eb668fa1c485633f89ef48236f9af
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2026-04-08 23:18:54 +0900

    [Gold IV] Title: 연구소, Time: 1608 ms, Memory: 35044 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,33 +1,32 @@
 import sys
 from collections import deque
-input = lambda:sys.stdin.readline().rstrip()
+input = lambda: sys.stdin.readline().rstrip()
 
 N, M = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(N)]
 
 dy = [-1, 0, 1, 0]
-dx = [0, -1, 0, 1]
+dx = [0, 1, 0, -1]
 
-def bfs(y, x, visited):
-    q = deque()
-    q.append((y, x))
+def spreadVirus(visited):
+    q = deque(virus)
+    for y, x in virus:
+        visited[y][x] = True
 
     while q:
         y, x = q.popleft()
         for i in range(4):
             ny = y + dy[i]
             nx = x + dx[i]
-            if ny < 0 or nx < 0 or ny >= N or nx >= M: continue
-            if visited[ny][nx] or graph[ny][nx] == 1: continue
-            q.append((ny, nx))
+            if ny < 0 or ny >= N or nx < 0 or nx >= M: continue
+            if graph[ny][nx] == 1 or visited[ny][nx]: continue
             visited[ny][nx] = True
+            q.append((ny, nx))
 
-def solve():
+def solution():
     visited = [[False] * M for _ in range(N)]
-    for vx, vy in virus:
-        visited[vx][vy] = True
-        bfs(vx, vy, visited)
-
+    spreadVirus(visited)
+    
     cnt = 0
     for i in range(N):
         for j in range(M):
@@ -36,16 +35,14 @@ def solve():
 
     return cnt
 
-if __name__ == "__main__":        
+if __name__ == "__main__":
     ret = 0
     wall, virus = [], []
+
     for i in range(N):
         for j in range(M):
-            if graph[i][j] == 0:
-                wall.append((i, j))
-            if graph[i][j] == 2:
-                virus.append((i, j))
-
+            if graph[i][j] == 0: wall.append((i, j))
+            if graph[i][j] == 2: virus.append((i, j))
 
     for i in range(len(wall)):
         for j in range(i):
@@ -53,7 +50,7 @@ if __name__ == "__main__":
                 graph[wall[i][0]][wall[i][1]] = 1
                 graph[wall[j][0]][wall[j][1]] = 1
                 graph[wall[k][0]][wall[k][1]] = 1
-                ret = max(ret, solve())
+                ret = max(ret, solution())
                 graph[wall[i][0]][wall[i][1]] = 0
                 graph[wall[j][0]][wall[j][1]] = 0
                 graph[wall[k][0]][wall[k][1]] = 0
