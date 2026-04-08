3be9d0c15fd89e6d640044cdaad249a06e7a3b41
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2026-04-08 11:17:52 +0900

    [Silver I] Title: 영역 구하기, Time: 44 ms, Memory: 35080 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,14 +1,13 @@
 import sys
+sys.setrecursionlimit(10**7)
 input = lambda: sys.stdin.readline().rstrip()
-sys.setrecursionlimit(10 ** 6)
 
-M, N, K =map(int, input().split())
-graph = [[0] * N for _ in range(M)]
-visited = [[0] * N for _ in range(M)]
+N, M, K = map(int, input().split())
+graph = [[0 for _ in range(M)] for _ in range(N)]
 
-ret = []
 dy = [-1, 0, 1, 0]
-dx = [0, -1, 0, 1]
+dx = [0, 1, 0, -1]
+ret = []
 
 def dfs(y, x):
     cnt = 1
@@ -17,23 +16,23 @@ def dfs(y, x):
     for i in range(4):
         ny = y + dy[i]
         nx = x + dx[i]
-        if ny < 0 or nx < 0 or ny >= M or nx >= N: continue
+        if ny < 0 or ny >= N or nx < 0 or nx >= M: continue
         if graph[ny][nx] == 1: continue
-        graph[ny][nx] = 1
         cnt += dfs(ny, nx)
-    
+
     return cnt
 
-for _ in range(K):
-    x1, y1, x2, y2 = map(int, input().split())
+if __name__ == "__main__":
+    for _ in range(K):
+        x1, y1, x2, y2 = map(int, input().split())
+        for i in range(y1, y2):
+            for j in range(x1, x2):
+                graph[i][j] = 1
 
-    for i in range(y1, y2):
-        for j in range(x1, x2):
-            graph[i][j] = 1
+    for i in range(N):
+        for j in range(M):
+            if graph[i][j]: continue
 
-for i in range(M):
-    for j in range(N):
-        if graph[i][j] == 0:
             ret.append(dfs(i, j))
 
 ret.sort()
