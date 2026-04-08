e37febf5c2603299e6e29dfc818d96018da4dcff
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2026-04-08 22:36:13 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 72 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -1,23 +1,29 @@
 import sys
+input = lambda: sys.stdin.readline().rstrip()
 
-while 1:
-    s = sys.stdin.readline().rstrip()
-    if s == '.':
+while True:
+    s = input()
+    if s == ".":
         break
-        
-    stack = []
-    for i in s:
-        if i not in '()[]':
-            continue
-        if i == '(' or i == '[':
-            stack.append(i)
-        elif (i == ')' and stack and stack[-1] == '(') or (i == ']' and stack and stack[-1] == '['):
-            stack.pop()
-        else:
-            stack.append(-1)
-            break
-            
-    if stack:
-        print('no')
-    else:
-        print('yes')
+
+    stk = []
+    flag = True
+    for c in s:
+        if c == ")":
+            if not stk or stk[-1] == "[":
+                flag = False
+                break
+            else:
+                stk.pop()
+
+        if c == "]":
+            if not stk or stk[-1] == "(":
+                flag = False
+                break
+            else:
+                stk.pop()
+
+        if c == "(" or c == "[": stk.append(c)
+    
+    if flag and not stk: print("yes")
+    else: print("no")
