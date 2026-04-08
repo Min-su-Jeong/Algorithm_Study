7b9366600c53d658248048ae6898dd820c6c7f3f
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2026-04-08 22:15:32 +0900

    [Silver III] Title: NBA 농구, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2852. NBA 농구/NBA 농구.py
+++ 백준/Silver/2852. NBA 농구/NBA 농구.py
@@ -1,38 +1,36 @@
-def timeToInt(s: str):
-    h, m = map(int, s.split(':'))
-    return h * 60 + m
-
 def intToTime(i: int):
     h = i // 60
     m = i % 60
     return f"{h:02d}:{m:02d}"
 
-def calcTime(time):
+def timeToInt(s: str):
+    h, m = map(int, s.split(':'))
+    return h * 60 + m
+
+def addTime(time):
     return timeToInt(time) - timeToInt(prev)
 
 if __name__ == "__main__":
     N = int(input())
 
+    ret1, ret2 = 0, 0
     t1, t2 = 0, 0
-    sum1, sum2 = 0, 0
     prev = ""
 
     for _ in range(N):
         tno, time = input().split()
-        tno = int(tno)
 
         if t1 > t2:
-            sum1 += calcTime(time)
+            ret1 += addTime(time)
         elif t1 < t2:
-            sum2 += calcTime(time)
+            ret2 += addTime(time)
 
-        if tno == 1: t1 += 1
+        if tno == "1": t1 += 1
         else: t2 += 1
-
         prev = time
 
-    if t1 > t2: sum1 += calcTime("48:00")
-    elif t1 < t2: sum2 += calcTime("48:00")
+    if t1 > t2: ret1 += addTime("48:00")
+    elif t1 < t2: ret2 += addTime("48:00")
 
-    print(intToTime(sum1))
-    print(intToTime(sum2))
+    print(intToTime(ret1))
+    print(intToTime(ret2))
