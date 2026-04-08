0f53124d06300f22b66cf26d47403abfe2747309
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2026-04-08 14:14:00 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.py
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.py
@@ -1,30 +1,39 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+def check(string):
+    prev = string[0]
+    includeV, flag = False, True
+    ccnt, vcnt = 0, 0
+
+    for idx, s in enumerate(string):
+        if s in "aeiou":
+            includeV = True
+            vcnt += 1
+            ccnt = 0
+        else:
+            ccnt += 1
+            vcnt = 0
+
+        if ccnt == 3 or vcnt == 3:
+            flag = False
+
+        if idx >= 1 and prev == s and (s != "e" and s != "o"):
+            flag = False
+        
+        prev = s
+
+    if not includeV: return False
+    if not flag: return False
+    return True
+
 while True:
     s = input()
     if s == "end":
         break
     
-    prev = -1
-    vcnt, ccnt = 0, 0
-    avail, includeV = True, False
-
-    for i in range(len(s)):
-        idx = s[i]
-        if s[i] in "aeiou":
-            vcnt += 1
-            ccnt = 0
-            includeV = True
-        else:
-            vcnt = 0
-            ccnt += 1
-
-        if vcnt == 3 or ccnt == 3:
-            avail = False
-
-        if i >= 1 and prev == idx and (idx != 'e' and idx != 'o'): avail = False
-        prev = idx
-
-    if not includeV: avail = False
-    if not avail:
-        print(f"<{s}> is not acceptable.")
-    else:
+    ret = check(s)
+    if ret:
         print(f"<{s}> is acceptable.")
+    else:
+        print(f"<{s}> is not acceptable.")
