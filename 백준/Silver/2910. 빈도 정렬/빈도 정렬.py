13b4881d6d7eddebf4854d3392ed89284e3ef487
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2026-04-08 13:47:26 +0900

    [Silver III] Title: 빈도 정렬, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.py
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.py
@@ -1,18 +1,13 @@
-import sys
-input = lambda: sys.stdin.readline().rstrip()
-
 N, C = map(int, input().split())
-arr = list(map(int, input().split()))
+msg = list(map(int, input().split()))
 
-idx = {}
-freq = {}
-for i, x in enumerate(arr):
-    freq[x] = freq.get(x, 0) + 1
-    if x not in idx:
-        idx[x] = i
+freq, idx = {}, {}
+for i, m in enumerate(msg):
+    freq[m] = freq.get(m, 0) + 1
+    if m not in idx:
+        idx[m] = i
 
 v = list(freq.keys())
-
 v.sort(key=lambda x: (-freq[x], idx[x]))
 
 for i in v:
