5b8a80b6b0f83772c609f8a67833432b3a8b9d91
Min-su-Jeong <74342121+Min-su-Jeong@users.noreply.github.com>
2026-04-09 12:46:35 +0900

    [Gold IV] Title: 치즈, Time: 76 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.py
+++ 백준/Gold/2636. 치즈/치즈.py
@@ -6,43 +6,43 @@ N, M = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(N)]
 
 dy = [-1, 0, 1, 0]
-dx = [0, -1, 0, 1]
+dx = [0, 1, 0, -1]
 
 def melt():
+    melted = []
     visited = [[False] * M for _ in range(N)]
+    visited[0][0] = True
+
     q = deque()
     q.append((0, 0))
-    visited[0][0] = True
-    melted = []
-
+    
     while q:
         y, x = q.popleft()
         for i in range(4):
             ny = y + dy[i]
             nx = x + dx[i]
-            if ny < 0 or nx < 0 or ny >= N or nx >= M: continue
-            if visited[ny][nx]: continue
+            if ny < 0 or ny >= N or nx < 0 or nx >= M or visited[ny][nx]: continue
             visited[ny][nx] = True
             if graph[ny][nx] == 1:
                 melted.append((ny, nx))
             else:
                 q.append((ny, nx))
-    
+
     for y, x in melted:
         graph[y][x] = 0
 
     return len(melted)
 
 if __name__ == "__main__":
-    ret1, ret2 = 0, 0
+    time, area = 0, 0
 
     while True:
         melted = melt()
         if melted == 0:
             break
         
-        ret1 += 1
-        ret2 = melted
-    
-    print(ret1)
-    print(ret2)
+        time += 1
+        area = melted
+
+    print(time)
+    print(area)
